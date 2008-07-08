2d8105c6d1ebb40fc7efb844ad744bab69edf51a
maxence <devnull@localhost>
2008-07-08 20:55:02 +0200

    [svn r38] Translate database name from french to english

--- ui/base.sql
+++ ui/base.sql
@@ -13,23 +13,23 @@
 -- --------------------------------------------------------
 
 -- 
--- Structure de la table `RElementsSalles`
+-- Structure de la table `RItemsRooms`
 -- 
 
-CREATE TABLE `RElementsSalles` (
+CREATE TABLE `R_ITEMS_ROOMS` (
   `id` int(11) NOT NULL auto_increment,
-  `element` int(11) NOT NULL,
-  `salle` int(11) NOT NULL,
+  `item` int(11) NOT NULL,
+  `room` int(11) NOT NULL,
   PRIMARY KEY  (`id`),
-  KEY `FK_ELEMENT` (`element`),
-  KEY `FK_SALLE` (`salle`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+  KEY `FK_ELEMENT` (`item`),
+  KEY `FK_ROOM` (`room`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=10 ;
 
 -- 
--- Contenu de la table `RElementsSalles`
+-- Contenu de la table `RItemsRooms`
 -- 
 
-INSERT INTO `RElementsSalles` (`id`, `element`, `salle`) VALUES 
+INSERT INTO `R_ITEMS_ROOMS` (`id`, `item`, `room`) VALUES 
 (1, 1, 1),
 (2, 2, 1),
 (3, 3, 2),
@@ -40,64 +40,64 @@ INSERT INTO `RElementsSalles` (`id`, `element`, `salle`) VALUES
 -- --------------------------------------------------------
 
 -- 
--- Structure de la table `RThermometreNom`
+-- Structure de la table `RThermometerName`
 -- 
 
-CREATE TABLE `RThermometreNom` (
+CREATE TABLE `R_THERMOMETER_NAME` (
   `id` int(11) NOT NULL auto_increment,
   `id_thermo` varchar(16) NOT NULL,
   `label` varchar(50) NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `id_thermo` (`id_thermo`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- 
--- Contenu de la table `RThermometreNom`
+-- Contenu de la table `RThermometerNom`
 -- 
 
 
 -- --------------------------------------------------------
 
 -- 
--- Structure de la table `RThermometreSalles`
+-- Structure de la table `RThermometerRooms`
 -- 
 
-CREATE TABLE `RThermometreSalles` (
+CREATE TABLE `R_THERMOMETER_ROOMS` (
   `id` int(11) NOT NULL auto_increment,
-  `thermometre` varchar(16) NOT NULL,
-  `id_salle` int(11) NOT NULL,
+  `thermometer` varchar(16) NOT NULL,
+  `id_room` int(11) NOT NULL,
   PRIMARY KEY  (`id`),
-  KEY `FK_THERMOMETRE` (`thermometre`),
-  KEY `FK_SALLE` (`id_salle`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  KEY `FK_THERMOMETER` (`thermometer`),
+  KEY `FK_ROOM` (`id_room`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 -- 
--- Contenu de la table `RThermometreSalles`
+-- Contenu de la table `RThermometerRooms`
 -- 
 
-INSERT INTO `RThermometreSalles` (`id`, `thermometre`, `id_salle`) VALUES 
+INSERT INTO `R_THERMOMETER_ROOMS` (`id`, `thermometer`, `id_room`) VALUES 
 (1, '/28.12BB4B010000', 1);
 
 -- --------------------------------------------------------
 
 -- 
--- Structure de la table `Treleves`
+-- Structure de la table `Tstatements`
 -- 
 
-CREATE TABLE `Treleves` (
+CREATE TABLE `T_STATEMENTS` (
   `id` int(11) NOT NULL auto_increment,
   `date` timestamp NOT NULL default CURRENT_TIMESTAMP,
-  `thermometre` varchar(16) NOT NULL,
+  `thermometer` varchar(16) NOT NULL,
   `temperature` float NOT NULL,
   PRIMARY KEY  (`id`),
-  KEY `thermometre` (`thermometre`(1))
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=2842 ;
+  KEY `thermometer` (`thermometer`(1))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=2842 ;
 
 -- 
--- Contenu de la table `Treleves`
+-- Contenu de la table `Tstatements`
 -- 
 
-INSERT INTO `Treleves` (`id`, `date`, `thermometre`, `temperature`) VALUES 
+INSERT INTO `T_STATEMENTS` (`id`, `date`, `thermometer`, `temperature`) VALUES 
 (6, '2008-06-14 05:29:26', '/28.12BB4B010000', 23.2323),
 (7, '2008-06-14 05:34:26', '/28.12BB4B010000', 23.2323),
 (8, '2008-06-14 05:39:26', '/28.12BB4B010000', 23.875),
@@ -107,70 +107,70 @@ INSERT INTO `Treleves` (`id`, `date`, `thermometre`, `temperature`) VALUES
 -- --------------------------------------------------------
 
 -- 
--- Structure de la table `capacites`
+-- Structure de la table `capacities`
 -- 
 
-CREATE TABLE `capacites` (
+CREATE TABLE `CAPACITIES` (
   `id` int(11) NOT NULL auto_increment,
   `id_piece` int(11) NOT NULL,
-  `capacite` enum('temperature','lumiere','musique') NOT NULL,
+  `capacity` enum('temperature','light','music') NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `FK_PIECE` (`id_piece`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 -- 
--- Contenu de la table `capacites`
+-- Contenu de la table `capacities`
 -- 
 
-INSERT INTO `capacites` (`id`, `id_piece`, `capacite`) VALUES 
+INSERT INTO `CAPACITIES` (`id`, `id_piece`, `capacity`) VALUES 
 (2, 1, 'temperature'),
-(3, 2, 'lumiere'),
-(4, 1, 'musique');
+(3, 2, 'light'),
+(4, 1, 'music');
 
 -- --------------------------------------------------------
 
 -- 
--- Structure de la table `elements`
+-- Structure de la table `items`
 -- 
 
-CREATE TABLE `elements` (
+CREATE TABLE `ITEMS` (
   `id` int(11) NOT NULL auto_increment,
-  `nom` varchar(30) NOT NULL,
+  `name` varchar(30) NOT NULL,
   `description` varchar(30) NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
 
 -- 
--- Contenu de la table `elements`
+-- Contenu de la table `items`
 -- 
 
-INSERT INTO `elements` (`id`, `nom`, `description`) VALUES 
+INSERT INTO `ITEMS` (`id`, `name`, `description`) VALUES 
 (1, 'A1', 'lampe'),
 (2, 'A2', 'chambre'),
 (3, 'A3', 'guirlande'),
 (4, 'A4', 'cafetière'),
-(5, 'A5', 'musique');
+(5, 'A5', 'music');
 
 -- --------------------------------------------------------
 
 -- 
--- Structure de la table `etats`
+-- Structure de la table `states`
 -- 
 
-CREATE TABLE `etats` (
+CREATE TABLE `STATES` (
   `id` int(11) NOT NULL auto_increment,
-  `element` int(11) NOT NULL,
-  `etat` tinyint(1) NOT NULL,
+  `item` int(11) NOT NULL,
+  `state` tinyint(1) NOT NULL,
   `date` timestamp NOT NULL default CURRENT_TIMESTAMP,
   PRIMARY KEY  (`id`),
-  KEY `element` (`element`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+  KEY `item` (`item`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
 
 -- 
--- Contenu de la table `etats`
+-- Contenu de la table `states`
 -- 
 
-INSERT INTO `etats` (`id`, `element`, `etat`, `date`) VALUES 
+INSERT INTO `STATES` (`id`, `item`, `state`, `date`) VALUES 
 (1, 1, 1, '2008-05-20 21:12:22'),
 (2, 2, 1, '2008-05-20 21:12:30'),
 (3, 3, 0, '2008-05-20 23:05:44'),
@@ -180,90 +180,90 @@ INSERT INTO `etats` (`id`, `element`, `etat`, `date`) VALUES
 -- --------------------------------------------------------
 
 -- 
--- Structure de la table `musique`
+-- Structure de la table `music`
 -- 
 
-CREATE TABLE `musique` (
+CREATE TABLE `MUSIC` (
   `id` int(11) NOT NULL auto_increment,
   `id_piece` int(11) NOT NULL,
   `titre` varchar(150) NOT NULL,
   `temps` time NOT NULL,
   `temps_actuel` time NOT NULL,
-  `etat` enum('play','pause','stop') NOT NULL,
+  `state` enum('play','pause','stop') NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 -- 
--- Contenu de la table `musique`
+-- Contenu de la table `music`
 -- 
 
-INSERT INTO `musique` (`id`, `id_piece`, `titre`, `temps`, `temps_actuel`, `etat`) VALUES 
+INSERT INTO `MUSIC` (`id`, `id_piece`, `titre`, `temps`, `temps_actuel`, `state`) VALUES 
 (1, 1, 'Ma zoulie chanson', '00:03:17', '00:01:05', 'stop');
 
 -- --------------------------------------------------------
 
 -- 
--- Structure de la table `salles`
+-- Structure de la table `rooms`
 -- 
 
-CREATE TABLE `salles` (
+CREATE TABLE `ROOMS` (
   `id` int(11) NOT NULL auto_increment,
-  `nom` varchar(30) NOT NULL,
+  `name` varchar(30) NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 -- 
--- Contenu de la table `salles`
+-- Contenu de la table `rooms`
 -- 
 
-INSERT INTO `salles` (`id`, `nom`) VALUES 
+INSERT INTO `ROOMS` (`id`, `name`) VALUES 
 (1, 'chambre'),
-(2, 'salle de bain');
+(2, 'room de bain');
 
 -- --------------------------------------------------------
 
 -- 
--- Structure de la table `VEtatsElements`
+-- Structure de la table `VStatesItems`
 -- 
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `domogik`.`VEtatsElements` AS select `EL`.`nom` AS `nom`,`ET`.`etat` AS `etat`,`ET`.`date` AS `date`,`EL`.`description` AS `description` from (`domogik`.`etats` `ET` join `domogik`.`elements` `EL`) where (`ET`.`element` = `EL`.`id`);
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `V_STATES_ITEMS` AS select `EL`.`name` AS `name`,`ET`.`state` AS `state`,`ET`.`date` AS `date`,`EL`.`description` AS `description` from (`STATES` `ET` join `ITEMS` `EL`) where (`ET`.`item` = `EL`.`id`);
 
 -- --------------------------------------------------------
 
 -- 
--- Structure de la table `VRelevesSalles`
+-- Structure de la table `VStatementsRooms`
 -- 
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`domogik`@`%` SQL SECURITY DEFINER VIEW `domogik`.`VRelevesSalles` AS select `TR`.`thermometre` AS `thermometre`,`TR`.`temperature` AS `temperature`,`TR`.`date` AS `date`,`S`.`nom` AS `nom` from ((`domogik`.`Treleves` `TR` join `domogik`.`RThermometreSalles` `TS`) join `domogik`.`salles` `S`) where ((`TR`.`thermometre` = `TS`.`thermometre`) and (`TS`.`id_salle` = `S`.`id`));
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`%` SQL SECURITY DEFINER VIEW `V_STATEMENTS_ROOMS` AS select `TR`.`thermometer` AS `thermometer`,`TR`.`temperature` AS `temperature`,`TR`.`date` AS `date`,`S`.`name` AS `name` from ((`T_STATEMENTS` `TR` join `R_THERMOMETER_ROOMS` `TS`) join `ROOMS` `S`) where ((`TR`.`thermometer` = `TS`.`thermometer`) and (`TS`.`id_room` = `S`.`id`));
 
 -- --------------------------------------------------------
 
 -- 
--- Structure de la table `vue`
+-- Structure de la table `VItemsRooms`
 -- 
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `domogik`.`vue` AS select `domogik`.`elements`.`nom` AS `nomE`,`domogik`.`salles`.`nom` AS `nomS` from ((`domogik`.`elements` join `domogik`.`salles`) join `domogik`.`RElementsSalles`) where ((`domogik`.`elements`.`id` = `domogik`.`RElementsSalles`.`element`) and (`domogik`.`RElementsSalles`.`salle` = `domogik`.`salles`.`id`));
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `V_ITEMS_ROOMS` AS select `ITEMS`.`name` AS `nameI`,`ROOMS`.`name` AS `nameR` from ((`ITEMS` join `ROOMS`) join `R_ITEMS_ROOMS`) where ((`ITEMS`.`id` = `R_ITEMS_ROOMS`.`item`) and (`R_ITEMS_ROOMS`.`room` = `ROOMS`.`id`));
 
 -- 
 -- Contraintes pour les tables exportées
 -- 
 
 -- 
--- Contraintes pour la table `RElementsSalles`
+-- Contraintes pour la table `RItemsRooms`
 -- 
-ALTER TABLE `RElementsSalles`
-  ADD CONSTRAINT `RElementsSalles_ibfk_7` FOREIGN KEY (`element`) REFERENCES `elements` (`id`) ON DELETE NO ACTION,
-  ADD CONSTRAINT `RElementsSalles_ibfk_8` FOREIGN KEY (`salle`) REFERENCES `salles` (`id`) ON DELETE NO ACTION;
+ALTER TABLE `R_ITEMS_ROOMS`
+  ADD CONSTRAINT `R_ITEMS_ROOMS_ibfk_7` FOREIGN KEY (`item`) REFERENCES `ITEMS` (`id`) ON DELETE NO ACTION,
+  ADD CONSTRAINT `R_ITEMS_ROOMS_ibfk_8` FOREIGN KEY (`room`) REFERENCES `ROOMS` (`id`) ON DELETE NO ACTION;
 
 -- 
--- Contraintes pour la table `capacites`
+-- Contraintes pour la table `capacities`
 -- 
-ALTER TABLE `capacites`
-  ADD CONSTRAINT `capacites_ibfk_1` FOREIGN KEY (`id_piece`) REFERENCES `salles` (`id`);
+ALTER TABLE `CAPACITIES`
+  ADD CONSTRAINT `CAPACITIES_ibfk_1` FOREIGN KEY (`id_piece`) REFERENCES `ROOMS` (`id`);
 
 -- 
--- Contraintes pour la table `etats`
+-- Contraintes pour la table `states`
 -- 
-ALTER TABLE `etats`
-  ADD CONSTRAINT `etats_ibfk_1` FOREIGN KEY (`element`) REFERENCES `elements` (`id`);
+ALTER TABLE `STATES`
+  ADD CONSTRAINT `STATES_ibfk_1` FOREIGN KEY (`item`) REFERENCES `ITEMS` (`id`);
 
