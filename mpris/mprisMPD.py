674503df059e355e7ae68c49171a3c45013b57cf
maxence <devnull@localhost>
2008-07-17 18:00:49 +0200

    [svn r47] Mpris MPD update

--- mpris/mprisMPD.py
+++ mpris/mprisMPD.py
@@ -71,7 +71,7 @@ class TrackList(dbus.service.Object):
         """
         Number of elements in the TrackList
         """
-        return self.__mpd.playlistinfo().__len__()
+        return self.__mpd.status['playlistlength']
 
     @dbus.service.method(dbus_interface='org.freedesktop.MediaPlayer')        
     def AddTrack(self, uri, playImmediately):
@@ -92,22 +92,31 @@ class TrackList(dbus.service.Object):
         """
         Removes an URI from the TrackList.
         """
-        pass
+        try:
+            self.__mpd.deleteid(track_position)
+        except:
+            pass
         
     @dbus.service.method(dbus_interface='org.freedesktop.MediaPlayer')    
     def SetLoop(self, loop):
         """
         Toggle playlist loop
         """
-        pass
+        if loop:
+            self.__mpd.repeat(1)
+        else:
+            self.__mpd.repeat(0)
         
     @dbus.service.method(dbus_interface='org.freedesktop.MediaPlayer')
     def SetRandom(self, random):
         """
         Toggle playlist shuffle / random. It may or may not play tracks only once. 
         """
-        pass
-    
+        if random:
+            self.__mpd.random(1)
+        else:
+            self.__mpd.random(0)
+
 class Player(dbus.service.Object):
 
     def __init__(self, object_path):
@@ -119,35 +128,35 @@ class Player(dbus.service.Object):
         """
         Goes to the next element
         """
-        pass
+        self.__mpd.next()
     
     @dbus.service.method(dbus_interface='org.freedesktop.MediaPlayer')    
     def Prev(self):
         """
         Goes to the previous element
         """
-        pass
+        self.__mpd.previous()
     
     @dbus.service.method(dbus_interface='org.freedesktop.MediaPlayer')    
     def Pause(self):
         """
         If playing : pause. If paused : unpause
         """
-        pass
+        self.__mpd.pause()
     
     @dbus.service.method(dbus_interface='org.freedesktop.MediaPlayer')
     def Stop(self):
         """
         Stop playing
         """
-        pass
+        self.__mpd.stop()
     
     @dbus.service.method(dbus_interface='org.freedesktop.MediaPlayer')
     def Play(self):
         """
         If playing : rewind to the beginning of current track, else : start playing
         """
-        pass
+        self.__mpd.play()
     
     @dbus.service.method(dbus_interface='org.freedesktop.MediaPlayer')
     def Repeat(self, repeat):
@@ -165,35 +174,72 @@ class Player(dbus.service.Object):
             Third integer: 0 = Go to the next element once the current has finished playing , 1 = Repeat the current element
             Fourth integer: 0 = Stop playing once the last element has been played, 1 = Never give up playing
         """
-        pass
+        state = {'play':0,'pause':1,'stop':2}
+        status = self.__mpd.status()
+        return (state[status['state']], int(state['random']), 0, int(state['repeat']))
     
     @dbus.service.method(dbus_interface='org.freedesktop.MediaPlayer')    
     def GetMetadata(self):
         """
         Gives all meta data available for the currently played element 
         """
-        pass
+        return self.__mpd.currentsong()
     
     @dbus.service.method(dbus_interface='org.freedesktop.MediaPlayer')    
     def GetCaps(self):
         """
         Return the "media player"'s current capabilities
+            * CAN_GO_NEXT	There is a current next track, or at least something that equals to it 
+              (that is, the remote can call the 'Next' method on the interface, and expect something to happen, heh)
+            * CAN_GO_PREV	Same as for NEXT, just previous track/something
+            * CAN_PAUSE	Can currently pause. This might not always be possible, and is yet another hint for frontends as to what to indicate
+            * CAN_PLAY	Whether playback can currently be started. 
+              This might not be the case if e.g. the playlist is empty in a player, 
+              or similar conditions. Here, again, it is entirely up to the player to decide when it can play or not, 
+              and it should signalize this using the caps API.
+            * CAN_SEEK	Whether seeking is possible with the currently played stream (UIs/frontends can then enable/disable seeking controls)
+            * CAN_PROVIDE_METADATA	Whether metadata can be acquired for the currently played stream/source using GetMetadata at all.
+            * CAN_HAS_TRACKLIST
+            	Whether the media player can hold a list of several items 
         """
-        pass
+        res = 0
+        #CAN_GO_NEXT
+        if ( int(self.__mpd.currentsong()['pos']) < int(self.__mpd.status()['playlistlength']) - 1 ) or ( self.__mpd.status()['repeat'] == 1 ) :
+            res += 2
+        #CAN_GO_PREV
+        if ( int(self.__mpd.currentsong()['pos']) > 0 or ( self.__mpd.status()['repeat'] == 1 ) :
+            res += 4
+        #CAN_PAUSE
+        if ( self.__mpd.status['state'] != 'stop' ):
+            res += 8
+        #CAN_PLAY
+        if ( self.__mpd.status['playlistlength'] != 0 ):
+            res += 16
+        #CAN_SEEK
+        #Can't find why we couldn't seek, except if there is no current song
+        if ( self.__mpd.currentsong() != {} ):
+            res += 32
+        #CAN_PROVIDE_METADATA
+        #Why couldn't we provide this ?
+            res += 64
+        #CAN_HAS_TRACKLIST
+        #Why couldn't we provide this ?
+            res += 128
+        return res
     
     @dbus.service.method(dbus_interface='org.freedesktop.MediaPlayer')
     def VolumeSet(self, level):
         """
         Sets the volume (argument must be in [0;100]) 
         """
-        pass
+        self.__mpd.setvol(level)
     
     @dbus.service.method(dbus_interface='org.freedesktop.MediaPlayer')    
     def VolumeGet(self):
         """
         Returns the current volume (must be in [0;100])
         """
-        pass
+        self.__mpd.status()['volume']
         
     
     @dbus.service.method(dbus_interface='org.freedesktop.MediaPlayer')    
@@ -201,14 +247,14 @@ class Player(dbus.service.Object):
         """
         Sets the playing position (argument must be in [0;<track_length>] in milliseconds) 
         """
-        pass
+        self.__mpd.seekid(int(self.__mpd.currentsong()['id']), position)
     
     @dbus.service.method(dbus_interface='org.freedesktop.MediaPlayer')
     def PositionGet(self):
         """
         Returns the playing position (will be [0;<track_length>] in milliseconds) 
         """
-        pass
+        return self.__mpd.status()['time'].split(':')[0]
     
     @dbus.service.signal(dbus_interface='org.freedesktop.MediaPlayer')    
     def TrackChange(self, items, values):
@@ -216,7 +262,11 @@ class Player(dbus.service.Object):
         Signal is emitted when the "Media Player" plays another "Track". 
         Argument of the signal is the metadata attached to the new "Track"
         """
-        pass
+        res = {}
+        for i in range(items.__len__()):
+            res[items[i]] = values[i]
+        return res
+            
     
     @dbus.service.signal(dbus_interface='org.freedesktop.MediaPlayer')    
     def StatusChange(self, state, random, repeat, loop):
@@ -224,14 +274,16 @@ class Player(dbus.service.Object):
         Signal is emitted when the status of the "Media Player" change. 
         The argument has the same meaning as the value returned by GetStatus.
         """ 
-        pass
+        state = {'play':0,'pause':1,'stop':2}
+        status = self.__mpd.status()
+        return (state[status['state']], int(state['random']), 0, int(state['repeat']))
     
     @dbus.service.signal(dbus_interface='org.freedesktop.MediaPlayer')    
     def CapsChange(self, capabilities):
         """
         Signal is emitted when the "Media Player" changes capabilities, see GetCaps method.
         """
-        pass
+        return 
         
     @dbus.service.signal(dbus_interface='org.freedesktop.MediaPlayer')    
     def  TrackListChange(self, elementsCount):
