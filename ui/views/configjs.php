43b47ab77f8ed522f4f771bfdd8b764944e9e77f
mschneider <devnull@localhost>
2008-07-19 19:07:15 +0200

    [svn r78] Fix misplaced SVN header

--- ui/views/configjs.php
+++ ui/views/configjs.php
@@ -1,9 +1,9 @@
+<script>
 /*
 	$LastChangedBy: mschneider $
-	$LastChangedDate: 2008-07-19 18:55:17 +0200 (sam. 19 juil. 2008) $
-	$LastChangedRevision: 76 $
+	$LastChangedDate: 2008-07-19 19:07:15 +0200 (sam. 19 juil. 2008) $
+	$LastChangedRevision: 78 $
 */
-<script>
 var JS_DIR = '<?=$this->config->item('JS_DIR')?>';
 var CSS_DIR = '<?=$this->config->item('CSS_DIR')?>';
 var IMAGES_DIR = '<?=$this->config->item('IMAGES_DIR')?>';
