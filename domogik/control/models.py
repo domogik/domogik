86260b6969aacc5ce631d95be0cd75c2e0823c04
mschneider <devnull@localhost>
2008-10-05 16:40:21 +0200

    [svn r121] Cosmetics : only use tabs to indent the code

--- domogik/control/models.py
+++ domogik/control/models.py
@@ -20,51 +20,51 @@
 # Author : Marc Schneider <marc@domogik.org>
 
 # $LastChangedBy: mschneider $
-# $LastChangedDate: 2008-10-05 16:38:05 +0200 (dim. 05 oct. 2008) $
-# $LastChangedRevision: 120 $
+# $LastChangedDate: 2008-10-05 16:40:21 +0200 (dim. 05 oct. 2008) $
+# $LastChangedRevision: 121 $
 
 from django.db import models
 
 class Item(models.Model):
-    name = models.CharField(max_length=30)
-    description = models.CharField(max_length=30)
+	name = models.CharField(max_length=30)
+	description = models.CharField(max_length=30)
 
 class Thermometer(models.Model):
-    thermometer = models.CharField(max_length=16)
-    label = models.CharField(max_length=50)
+	thermometer = models.CharField(max_length=16)
+	label = models.CharField(max_length=50)
 
 class Room(models.Model):
-    name = models.CharField(max_length=30)
-    items = models.ManyToManyField(Item) 
-    thermometers = models.ManyToManyField(Thermometer) 
+	name = models.CharField(max_length=30)
+	items = models.ManyToManyField(Item) 
+	thermometers = models.ManyToManyField(Thermometer) 
 
 class Capacity(models.Model):
-    CAPACITY_CHOICES = (
+	CAPACITY_CHOICES = (
 		('temperature', 'Temperature'),
 		('light', 'Light'),
 		('music', 'Music')
-    )
-    room = models.ForeignKey(Room)
-    capacity = models.CharField(max_length=30, choices = CAPACITY_CHOICES)
+	)
+	room = models.ForeignKey(Room)
+	capacity = models.CharField(max_length=30, choices = CAPACITY_CHOICES)
 
 class Music(models.Model):
-    STATE_CHOICES = (
+	STATE_CHOICES = (
 		('play', 'Play'),
 		('pause', 'Pause'),
 		('stop', 'Stop')
-    )
-    room = models.ForeignKey(Room)
-    title = models.CharField(max_length=150)
-    time = models.TimeField()
-    current_time = models.TimeField()
-    state = models.CharField(max_length=10, choices = STATE_CHOICES)
+	)
+	room = models.ForeignKey(Room)
+	title = models.CharField(max_length=150)
+	time = models.TimeField()
+	current_time = models.TimeField()
+	state = models.CharField(max_length=10, choices = STATE_CHOICES)
 
 class Statement(models.Model):
-    date = models.DateTimeField()
-    thermometer = models.ForeignKey(Thermometer)
-    temperature = models.FloatField()
+	date = models.DateTimeField()
+	thermometer = models.ForeignKey(Thermometer)
+	temperature = models.FloatField()
 
 class State(models.Model):
-    item = models.ForeignKey(Item)
-    state = models.SmallIntegerField() 
-    date = models.DateTimeField()
+	item = models.ForeignKey(Item)
+	state = models.SmallIntegerField() 
+	date = models.DateTimeField()
