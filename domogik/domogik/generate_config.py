10238aa79ceec152d2d5a3450bfa90ff51cf57dc
maxence <devnull@localhost>
2009-02-02 10:54:53 +0100

    [svn r314] add x10 to the config writer

--- domogik/domogik/generate_config.py
+++ domogik/domogik/generate_config.py
@@ -24,13 +24,14 @@
 # $LastChangedRevision: 301 $
 
 import re
+import sys
 from configobj import ConfigObj
 
 class ConfigManager():
     '''
     General config manager which can ask user about config settings, check results and write config file
     '''
-    def __init__(self, settings = []):
+    def __init__(self, settings = [], session = None):
         '''
         Constructor 
         @param settings List of tuples defining parameters to ask user for : {'name','question','check_regex',['proposed_val1','proposed_val2']}
@@ -38,12 +39,15 @@ class ConfigManager():
         Proposed value list can be None. If it isn't, the first value is took as default choice
         '''
         self._settings = settings
+        self._session = session
 
     def ask(self):
         '''
         Ask the user for each entry of self._settings
         '''
         result = {}
+        if self._session != None:
+            print "**** %s ****\n\n" % self._session.title()
         for entry in self._settings:
             name = entry[0]
             question = entry[1]
@@ -52,33 +56,35 @@ class ConfigManager():
             #Build the question
             req = question
             if proposed_val is not None:
-                req += " [%s" % (proposed_val[0].upper())
+                req += " [%s" % proposed_val[0]
                 for v in proposed_val[1:]:
-                    req += "/%s" % v
+                    req += "  %s" % v
                 req +=  "]"
             res_state_ok = False
+            req += " : "
             while not res_state_ok:
                 tmp_res = raw_input(req)
                 #Check the result
                 #If proposed value isn't empty
                 if proposed_val != None:
                     #If tmp_res is empty, we choose the default value
-                    if tmp_res == "":
+                    if tmp_res == "" or tmp_res == None:
                         final_res = proposed_val[0]
+                        tmp_res = proposed_val[0] #In case of regex check
                         res_state_ok = True
+        #            else:
+         #               if (tmp_res not in proposed_val) and (tmp_res.lower() not in proposed_val) and (tmp_res.upper() not in proposed_val):
+         #                   #Invalid entry
+         #                   pass
                     else:
-                        if (tmp_res not in proposed_val) and (tmp_res.lower() not in proposed_val) and (tmp_res.upper() not in proposed_val):
-                            #Invalid entry
-                            pass
-                        else:
-                            #Result is ok for this test
-                            res_state_ok = True
-                            final_res = tmp_res
+                        #Result is ok for this test
+                        res_state_ok = True
+                        final_res = tmp_res
                 #Check against regex
                 if check_re != None:
                     #WARNING : this line will throw an exception if the regex is badly formatted
                     r = re.compile(check_re)
-                    if (r.match(tmp_res)):
+                    if (r.match(str(tmp_res))):
                         #Result is ok for this test
                         res_state_ok = True
                         final_res = tmp_res
@@ -123,9 +129,51 @@ class generalConfig():
         ]
         file = "domogik.cfg"
         section = "domogik"
-        config = ConfigManager(informations)
+        config = ConfigManager(informations,section)
         result = config.ask()
         config.write(file, result, section)
 
+class genericPluginConfig():
+    '''
+    Generic list for plugins
+    '''
+    informations = [
+    ('address','What is the IP address the plugin must use ?',r"^([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])$",None),
+    ('port','What is the port the plugin must bind ?',r"^[1-9][0-9]+", None),
+    ('hub_address','What is the IP address the plugin must connect to ?',r"^([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])$",None),
+    ('hub_port','What is the port the plugin must connect to ?',r"^[1-9][0-9]+", [3865])
+    ]
+    def __init__():
+        raise NotImplementedException
+
+class x10Config(genericPluginConfig):
+    '''
+    Ask the user for specific config for X10 xPL module
+    '''
+    def __init__(self):
+        self.informations.extend([('heyu_cfg_file','What is the full path to Heyu config file ?', None, ['/etc/heyu/x10.cfg','/usr/local/etc/heyu/x10.cfg'])
+        ])
+        file = "conf.d/x10.cfg"
+        section = "x10"
+        config = ConfigManager(self.informations,section)
+        result = config.ask()
+        config.write(file, result, section)
+
+
 if __name__ == "__main__":
-    generalConfig()
+    if len(sys.argv) > 1:
+        choice = sys.argv[1]
+    else:
+        print "usage : generate_config.py config"
+        print " - config : name of the section to reconfigure"
+        print "            Can be one of : all, plugins, x10, 1wire"
+        exit(1)
+
+    if choice == 'all':
+        generalConfig()
+        x10Config()
+    elif choice == 'plugins':
+        x10Config() 
+    elif choice == 'x10':
+        x10Config() 
+
