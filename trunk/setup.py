3d458aa6de5ca32309266d933d61d7779648cdc8
Maxence Dunnewind <devnull@localhost>
2009-05-24 13:36:23 +0200

    add ez_setup.py

--- trunk/setup.py
+++ trunk/setup.py
@@ -3,14 +3,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Domogik',
-    version='0.5a',
-    url='http://www.domogik.org/',
-    author='OpenSource home automation software',
+    name = 'Domogik',
+    version = '0.1a',
+    url = 'http://www.domogik.org/',
+    author = 'OpenSource home automation software',
+    author_email = 'domogik-general@lists.labs.libre-entreprise.org',
     package_dir = {'': 'domogik'},
     packages=find_packages('domogik'),
 #    namespace_packages=['domogik',],
     include_package_data = True,
     install_requires=['setuptools','django'],
     zip_safe = False,
+    license = 'GPL v3',
     )
