daf5e80dec42e53fa20bd65175569a5c5b8ebafe
Maxence Dunnewind <devnull@localhost>
2009-05-25 15:34:53 +0200

    setup

--- trunk/setup.py
+++ trunk/setup.py
@@ -5,20 +5,33 @@ import ez_setup
 ez_setup.use_setuptools()
 
 #Main install part
+import os
 from setuptools import setup, find_packages
 
+def rec_glob_get_files(path):
+    d=[]
+    for i in os.listdir(path):
+        if not os.path.isdir(path+i):
+            d.append(path+i)
+        else:
+            d.extend(rec_glob_get_files(path+i))
+    return d
+
+res = rec_glob_get_files('domogik/ui/')
+print res
 setup(
     name = 'Domogik',
     version = '0.1a',
     url = 'http://www.domogik.org/',
     author = 'OpenSource home automation software',
     author_email = 'domogik-general@lists.labs.libre-entreprise.org',
-    package_dir = {'': 'domogik'},
-    packages=find_packages('domogik'),
+    packages=find_packages(),
+    package_dir = {'domogik':'domogik'},
 #    namespace_packages=['domogik',],
-    include_package_data = True,
-    install_requires=['setuptools','django'],
+    install_requires=['setuptools','django >=1.0'],
     zip_safe = False,
     license = 'GPL v3',
     scripts = ['domogik/bin/generate_config.py','domogik/bin/dmgstart.py'],
+    package_data = {'domogik':res},
     )
+
