d8bda5e0c06162544c86394bdccc19e4d6d8a756
Ferllings <cedric@domogik.org>
2010-01-06 20:57:48 +0100

    Added icons sources

--- src/domogik/ui/work/web/design/js/page-view.js
+++ src/domogik/ui/work/web/design/js/page-view.js
@@ -1,6 +1,5 @@
 $(function(){
    	$("#frame").attr('role', 'application');
-	$("#frame").addClass("ui-widget ui-widget-content");
 	$(".element-main").addClass("ui-widget ui-widget-content");
 });
 				
