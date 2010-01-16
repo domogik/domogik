e1e0d61b3523870daa5542d05f87fb087c891dcb
Ferllings <cedric@domogik.org>
2010-01-16 09:35:39 +0100

    Added binary command switch

---
+++ src/domogik/ui/djangodomo/core/templates/design/js/ui.action-binary.js
@@ -0,0 +1,37 @@
+function widgetmini_binary(widgetmini_id, function_id, widget_type) {
+    $('#' + widgetmini_id).addClass('widgetmini_binary')
+        .click(function () {process_binary(function_id)})
+        .keypress(function (e) {if (e.which == 13 || e.which == 32) {process_binary(function_id);}})
+        .addClass('icon32-widget-' + widget_type)
+        .addClass('binary_0')
+        .attr("tabindex", 0);
+}
+
+function widget_binary(widget_id, function_id, widget_type) {
+    $('#' + widget_id).addClass('widget_binary')
+        .addClass('icon32-widget-' + widget_type)
+        .addClass('binary_0');
+    $('#' + widget_id + " .on").click(function () {process_binary(function_id, 1)});
+    $('#' + widget_id + " .off").click(function () {process_binary(function_id, 0)});
+}
+
+function process_binary(function_id, force) {
+    $('#widgetmini_' + function_id).addClass('processing_state');
+    $('#widget_' + function_id).addClass('processing_state');
+    var status = (force == null) ? 1 : force; 
+    setTimeout("feedback_binary(" + function_id + ", " + status + ")",3000);
+}
+
+function feedback_binary(function_id, status) {
+    var widgetmini_id = 'widgetmini_' + function_id;
+    var widget_id = 'widget_' + function_id;
+    var oldstatus = (status == 0) ? 1 : 0 ;
+    $('#' + widgetmini_id).removeClass('processing_state')
+        .addClass('ok_state')
+        .removeClass('binary_' + oldstatus)
+        .addClass('binary_' + status);
+    $('#' + widget_id).removeClass('processing_state')
+        .addClass('ok_state')
+        .removeClass('binary_' + oldstatus)
+        .addClass('binary_' + status);
+}
