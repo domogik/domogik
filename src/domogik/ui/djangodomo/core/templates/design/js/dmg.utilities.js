5cdc047eb14e6dceac5a060204bed1c3cf42cc6d
Ferllings <cedric@domogik.org>
2010-04-29 08:57:56 +0200

    [django] Improved notifications system (close #153)

--- src/domogik/ui/djangodomo/core/templates/design/js/dmg.utilities.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-    $.extend({
-	    getJson: function(string){
-            var decoded = $('<div></div>').html(string).text();
-            console.debug(decoded); // Firebug
-            return eval('(' + decoded+ ')');
-        }
-    });
-})(jQuery);
--- src/domogik/ui/djangodomo/core/templates/design/js/ui.messages.js
+++
@@ -1,8 +0,0 @@
-
-function display_message(status, msg) {
-    if ($("#messages").length) {
-        $("#messages").removeClass().addClass(status.toLowerCase()).text(msg);
-    } else {
-        $("<div id='messages' class='" + status.toLowerCase() + " icon32-status-" + status.toLowerCase() + "'>" + msg + "</div>").insertAfter("h1");        
-    }
-}
