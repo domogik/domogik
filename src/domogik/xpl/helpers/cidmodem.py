e7e058988b9b1ab0a3b0702aaa58b9cd31f43549
Fritz <devnull@localhost>
2010-07-09 14:43:41 +0200

    Cidmodem : add helper

---
+++ src/domogik/xpl/helpers/cidmodem.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-                                                                           
+
+""" This file is part of B{Domogik} project (U{http://www.domogik.org}).
+
+License
+=======
+
+B{Domogik} is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+B{Domogik} is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Domogik. If not, see U{http://www.gnu.org/licenses}.
+
+Plugin purpose
+==============
+
+Get informations about mirror and ztamps
+
+Implements
+==========
+
+TODO
+
+@author: Fritz SMH <fritz.smh@gmail.com>
+@copyright: (C) 2007-2009 Domogik project
+@license: GPL(v3)
+@organization: Domogik
+"""
+
+from domogik.xpl.common.helper import Helper
+from domogik.xpl.common.helper import HelperError
+from domogik.xpl.lib.cidmodem import CallerIdModem 
+from domogik.xpl.lib.cidmodem import CallerIdModemException
+from domogik.common import logger
+
+
+class cidmodem(Helper):
+
+
+    def __init__(self):
+        """ Init Caller Id with a modem helper
+        """
+
+        self.commands =   \
+               { "read" : 
+                  {
+                    "cb" : self.read,
+                    "desc" : "Wait for a inbound call event",
+                    "min_args" : 1,
+                    "usage" : "read <device>"
+                  },
+                }
+
+        log = logger.Logger('cidmodem-helper')
+        self._log = log.get_logger()
+          
+
+
+    def find(self, args = None):
+        """ Try to find Mir:ror device
+        """
+        return ["TODO"]
+
+    def read(self, args = None):
+        """ Read Modem until an inbound call
+        """
+
+        # Init Mir:ror
+        cid  = CallerIdModem(self._log, None)
+        
+        # Open Modem
+        try:
+            cid.open(args[0])
+        except CallerIdModemException as e:
+            return [e.value]
+            
+        # read Modem
+        while True:
+            num = cid.read()
+            if num != None:
+                break
+
+        # Close Modem
+        try:
+            cid.close()
+        except CallerIdModemException as e:
+            return [e.value]
+
+        return ["Phone number : %s" % num]
+
+MY_CLASS = {"cb" : cidmodem}
+
+
+
+
