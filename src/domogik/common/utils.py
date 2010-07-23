42953f027258d12e646bad6026a2a30ab03ad07f
mirelsol <devnull@localhost>
2010-07-23 14:36:39 +0200

    Add utils module

---
+++ src/domogik/common/utils.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+""" This file is part of B{Domogik} project (U{http://www.domogik.org}).
+
+License
+=======
+
+B{Domogik} is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+B{Domogik} is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Domogik. If not, see U{http://www.gnu.org/licenses}.
+
+Module purpose
+==============
+
+Install the Domogik database based on config file values
+
+Implements
+==========
+
+
+@author: Maxence Dunnewind <maxence@dunnewind.net>
+@copyright: (C) 2007-2010 Domogik project
+@license: GPL(v3)
+@organization: Domogik
+"""
+
+
+def ucode(my_string):
+    """Convert a string into unicode or return None if None value is passed
+
+    @param my_string : string value to convert
+    @return a unicode string
+
+    """
+    if my_string is not None:
+        return unicode(my_string)
+    else:
+        return None
