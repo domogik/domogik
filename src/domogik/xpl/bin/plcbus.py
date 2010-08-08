41066e66319f56cf0557869477d93b35e4deb74e
Maxence Dunnewind <devnull@localhost>
2010-08-08 13:16:00 +0200

    PLCBUS fastpolling is used to detect new devices and check state changes

--- src/domogik/xpl/bin/plcbus.py
+++ src/domogik/xpl/bin/plcbus.py
@@ -37,12 +37,13 @@ Implements
 @organization: Domogik
 """
 
-from domogik.xpl.common.xplconnector import Listener
+from domogik.xpl.common.xplconnector import Listener, XplTimer
 from domogik.xpl.common.plugin import XplPlugin, XplResult
 from domogik.xpl.common.xplmessage import XplMessage
 from domogik.xpl.lib.plcbus import PLCBUSAPI
 from domogik.xpl.common.queryconfig import Query
-
+import threading
+import time
 
 class PlcBusMain(XplPlugin):
     ''' Manage PLCBus technology, send and receive order/state
@@ -64,10 +65,35 @@ class PlcBusMain(XplPlugin):
         res = XplResult()
         self._config.query('plcbus', 'device', res)
         device = res.get_value()['device']
+        res = XplResult()
+        self._config.query('plcbus', 'usercode', res)
+        self._usercode = res.get_value()['usercode']
+        res = XplResult()
+        self._config.query('plcbus', 'probe-interval', res)
+        self._probe_inter = int(res.get_value()['probe-interval'])
+        res = XplResult()
+        self._config.query('plcbus', 'probe-list', res)
+        self._probe_list = res.get_value()['probe-list']
+
         # Create log instance
         self._log = self.get_my_logger()
         self.api = PLCBUSAPI(self._log, device, self._command_cb, self._message_cb)
-        print "API : %s" % self.api
+        if self._probe_inter != 0:
+            self._probe_status = {}
+            self._probe_stop = threading.Event()
+            self._probe_thr = XplTimer(self._probe_inter, self._send_probe, self._probe_stop, self._myxpl)
+            self._probe_thr.start()
+            self.register_timer(self._probe_thr)
+
+    def _send_probe(self):
+        """ Send probe message 
+
+        """
+        for h in self._probe_list:
+            self.api.send("GET_ALL_ID_PULSE", h, self._usercode, 0, 0)
+            time.sleep(1)
+            self.api.send("GET_ALL_ON_ID_PULSE", h, self._usercode, 0, 0)
+            time.sleep(1)
 
     def _plcbus_cmnd_cb(self, message):
         '''
@@ -84,28 +110,60 @@ class PlcBusMain(XplPlugin):
             dev = message.data['device'].upper()
         if 'usercode' in message.data:
             user = message.data['usercode']
+        else:
+            user = self._usercode
         if 'data1' in message.data:
             level = message.data['data1']
         if 'data2' in message.data:
             rate = message.data['data2']
         self._log.debug("%s received : device = %s, user code = %s, level = "\
                 "%s, rate = %s" % (cmd.upper(), dev, user, level, rate))
-        if cmd == 'GET_ALL_ON_ID_PULSE':
-            self.api.get_all_on_id(user, dev)
-        else:
-            self.api.send(cmd.upper(), dev, user, level, rate)
+#        if cmd == 'GET_ALL_ON_ID_PULSE':
+#            self.api.get_all_on_id(user, dev)
+#        else:
+        self.api.send(cmd.upper(), dev, user, level, rate)
 
     def _command_cb(self, f):
         ''' Called by the plcbus library when a command has been sent.
         If the commands need an ack, this callback will be called only after the ACK has been received
         @param : plcbus frame as an array
         '''
-        mess = XplMessage()
-        mess.set_type('xpl-trig')
-        mess.set_schema('plcbus.basic')
-        mess.add_data({"usercode" : f["d_user_code"], "device": f["d_home_unit"],
-                       "command": f["d_command"], "data1": f["d_data1"], "data2": f["d_data2"]})
-        self._myxpl.send(mess)
+        if f["d_command"] == "GET_ALL_ID_PULSE":
+            data = int("%s%s" % (f["d_data1"], f["d_data2"]), 16)
+            house = f["d_home_unit"][0]
+            for i in range(0,16):
+                unit = data >> i & 1
+                code = "%s%s" % (house, i+1)
+                if unit and not code in self._probe_status:
+                    self._probe_status[code] = ""
+                    self._log.info("New device discovered : %s" % code)
+                elif (not unit) and code in self._probe_status:
+                    del self._probe_status[code]
+        elif f["d_command"] == "GET_ALL_ON_ID_PULSE":
+            data = int("%s%s" % (f["d_data1"], f["d_data2"]), 16)
+            house = f["d_home_unit"][0]
+            for i in range(0,16):
+                unit = data >> i & 1
+                code = "%s%s" % (house, i+1)
+                if code in self._probe_status and (self._probe_status[code] != str(unit)):
+                    self._probe_status[code] = str(unit)
+                    if unit == 1:
+                        command = "ON"
+                    else:
+                        command ="OFF"
+                    mess = XplMessage()
+                    mess.set_type('xpl-trig')
+                    mess.set_schema('plcbus.basic')
+                    mess.add_data({"usercode" : f["d_user_code"], "device": code,
+                                   "command": command})
+                    self._myxpl.send(mess)
+        else:
+            mess = XplMessage()
+            mess.set_type('xpl-trig')
+            mess.set_schema('plcbus.basic')
+            mess.add_data({"usercode" : f["d_user_code"], "device": f["d_home_unit"],
+                           "command": f["d_command"], "data1": f["d_data1"], "data2": f["d_data2"]})
+            self._myxpl.send(mess)
 
     def _message_cb(self, message):
         print "Message : %s " % message
