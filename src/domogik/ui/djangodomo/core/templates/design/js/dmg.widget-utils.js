9c214918b825d4afc37c4c24bbb21c61028b9cb9
Ferllings <cedric@domogik.org>
2010-08-28 22:50:09 +0200

    [ihm] moved command panel code to widget-utils

--- src/domogik/ui/djangodomo/core/templates/design/js/dmg.widget-utils.js
+++ src/domogik/ui/djangodomo/core/templates/design/js/dmg.widget-utils.js
@@ -7,6 +7,64 @@ $(function(){
             var y = c.y - (this.height() / 2);
             this.css('top', y).css('left', x);
             if (options.rotate) this.css('-moz-transform', 'rotate(' + options.deg + 'deg)').css('-webkit-transform', 'rotate(' + options.deg + 'deg)');
+        },
+        
+        panelAddCommand: function(options) {
+            var cx = this.width()/2;
+            var cy = this.height()/2;
+            var command = $("<div class='command'></div>");
+            command.addClass(options.class)
+                .click(options.click);
+            this.append(command);
+            command.moveToCircleCoord({x:cx, y:cy, r:options.r, deg:options.deg, rotate:options.rotate}); // ! Need to be after append
+        },
+        
+        panelAddText: function(options) {
+            var cx = this.width()/2;
+            var cy = this.height()/2;
+            var text = $("<div class='text'></div>");
+            text.addClass(options.class);
+            this.append(text);
+            text.moveToCircleCoord({x:cx, y:cy, r:options.r, deg:options.deg, rotate:options.rotate}); // ! Need to be after append
+        }
+    });
+    
+    $.extend({ 
+        getPanel: function(options) {
+            var panel = $("<div class='widget_1x1_panel'></div>");
+            panel.width(options.width)
+                .height(options.height)
+                .css('left', '-' + (options.width/2 - 25) + 'px')
+                .css('top', '-' + (options.height/2 - 25) + 'px');
+            var deco = $("<canvas class='deco' width='" + options.width + "' height='" + options.height + "'></canvas>")
+            panel.append(deco);
+            var canvas = deco.get(0);
+            if (canvas.getContext) {
+                var ctx = canvas.getContext('2d');
+                ctx.beginPath();
+                ctx.strokeStyle = "#BDCB2F";
+                ctx.lineWidth = 2;
+                ctx.arc(95,95,94,0,(Math.PI*2),true); // Outer circle  
+                ctx.stroke();
+                ctx.beginPath();
+                ctx.strokeStyle = "#000000";
+                ctx.lineWidth = 36;
+                ctx.lineCap = 'round';
+                ctx.arc(95,95,70,(Math.PI/180)*150,(Math.PI/180)*80,false); // Main circle  
+                ctx.stroke();
+
+                var c = getCircleCoord(95, 95, 65, 80);
+                ctx.beginPath();
+                ctx.strokeStyle = "#000000";
+                ctx.fillStyle = "#eeeeee";
+                ctx.lineWidth = 5;
+                ctx.lineCap = 'butt';
+                ctx.arc(c.x,c.y,20,0,(Math.PI*2),true); // Value circle  
+                ctx.fill();
+                ctx.stroke();
+            }
+
+            return panel;
         }
     });
 });
--- src/domogik/ui/djangodomo/core/templates/widgets/dmg_1X1_basicActuatorNumber/main.js
+++ src/domogik/ui/djangodomo/core/templates/widgets/dmg_1X1_basicActuatorNumber/main.js
@@ -22,49 +22,16 @@
             $(".openpanel", this.element).addClass("icon32-usage-" + o.usage)
                 .click(1000, function(e){self._panel.show();e.stopPropagation();})
                 .click(function(e){self.action();e.stopPropagation();});
-            this._panel = $("<div class='panel' width='190' height='190'></div>");
-            this._panel.append("<canvas class='deco' width='190' height='190'></canvas>")
-            this._panel.append("<div class='value'></div>")
-            this._panel.append("<div class='command increase'></div>")
-            this._panel.append("<div class='command decrease'></div>")
-            this._panel.append("<div class='command close'></div>")
-            
-            $(".increase", this._panel).click(function(e){self.increase();e.stopPropagation();});
-            $(".decrease", this._panel).click(function(e){self.decrease();e.stopPropagation();});
-            $(".close", this._panel).click(function(e){self._panel.hide();e.stopPropagation();});
-            var value = (o.model_parameters.valueMax - o.model_parameters.valueMin) / 2;
-
+                
+            this._panel = $.getPanel({width:190, height:190});
             this.element.append(this._panel);
-            $(".value", this._panel).moveToCircleCoord({x:95, y:95, r:65, deg:80});
-            $(".command.increase", this._panel).moveToCircleCoord({x:95, y:95, r:70, deg:-20, rotate:true});
-            $(".command.decrease", this._panel).moveToCircleCoord({x:95, y:95, r:70, deg:20, rotate:true});
-            $(".command.close", this._panel).moveToCircleCoord({x:95, y:95, r:70, deg:150, rotate:true});
+            this._panel.panelAddCommand({class:'increase', r:70, deg:-20, rotate:true, click:function(e){self.increase();e.stopPropagation();}});
+            this._panel.panelAddCommand({class:'decrease', r:70, deg:20, rotate:true, click:function(e){self.decrease();e.stopPropagation();}});
+            this._panel.panelAddCommand({class:'close', r:70, deg:150, rotate:true, click:function(e){self._panel.hide();e.stopPropagation();}});
+            this._panel.panelAddText({class:'value', r:65, deg:80});
             this._panel.hide();
-            var canvas = $(".deco", this._panel).get(0);
-            if (canvas.getContext) {
-                var ctx = canvas.getContext('2d');
-                ctx.beginPath();
-                ctx.strokeStyle = "#BDCB2F";
-                ctx.lineWidth = 2;
-                ctx.arc(95,95,94,0,(Math.PI*2),true); // Outer circle  
-                ctx.stroke();
-                ctx.beginPath();
-                ctx.strokeStyle = "#000000";
-                ctx.lineWidth = 36;
-                ctx.lineCap = 'round';
-                ctx.arc(95,95,70,(Math.PI/180)*150,(Math.PI/180)*80,false); // Main circle  
-                ctx.stroke();
 
-                var c = getCircleCoord(95, 95, 65, 80);
-                ctx.beginPath();
-                ctx.strokeStyle = "#000000";
-                ctx.fillStyle = "#eeeeee";
-                ctx.lineWidth = 5;
-                ctx.lineCap = 'butt';
-                ctx.arc(c.x,c.y,20,0,(Math.PI*2),true); // Value circle  
-                ctx.fill();
-                ctx.stroke();
-            }
+            var value = (o.model_parameters.valueMax - o.model_parameters.valueMin) / 2;
             this.setParameter(value);
         },
 
