cfd4cc412feb2c20b77326d7cde907c62ab07f04
Ferllings <cedric@domogik.org>
2010-11-28 21:20:05 +0100

    [ihm] Delete button for widgets, on edit page (close #610)

--- src/domogik/ui/djangodomo/core/templates/design/js/page-show-edit.js
+++ src/domogik/ui/djangodomo/core/templates/design/js/page-show-edit.js
@@ -26,31 +26,103 @@ $(function(){
 });
 
 (function($) {
-    function matchFilter(filters, id) {
-        var res = false;
-        if (filters) {
-            $.each(filters, function(index, filter){
-                var afilter = filter.split('.');
-                var aid = id.split('.');
-                res =  (afilter[0] == aid[0] || afilter[0] == '*') && (afilter[1] == aid[1] || afilter[1] == '*') && (afilter[2] == aid[2] || afilter[2] == '*');
-            });
-        } else {
-            res = true;
-        }
-        return res;
-    }
     
     function ondrop(event, ui) {
-        var item = ui.draggable.detach();
-        $(this).append(item);
-        item.removeAttr('style')
-            .addClass('success')
-            .draggable("option", 'helper', false)
-            .draggable("option", 'revert', false);
-        $.addAssociation(item, $(this), 'house', 0);            
+        var helper = ui.draggable.draggable( "option", "helper" );
+        var item = null;
+        if (helper == 'clone') {
+            item = $(ui.helper).clone();
+
+            $(this).append(item);
+            item.widget_model({
+                widgetid: ui.draggable.data('widgetid'),
+                featureid: ui.draggable.data('featureid'),
+                featurename: ui.draggable.data('featurename'),
+                devicename: ui.draggable.data('devicename'),
+                associationid: ui.draggable.data('associationid'),
+                draggable:{
+                    helper: false,
+                    revert: 'invalid',
+                    drag: ondrag,
+                    stop: onstop
+                },
+                deletable: true
+            });            
+        } else {
+            item = ui.draggable.detach();
+            $(this).append(item);
+            item.removeAttr('style');    
+        }
+        $.addAssociation(item, $(this));            
         return false;
     }
-        
+    
+    function onstop(event, ui) {
+        $("#panel").show();
+    }
+    
+    function ondrag(event, ui) {
+        $("#panel").hide();
+    }
+    
+    $.fn.extend({
+	    displayName: function(){
+            var devicename = this.data('devicename');
+            var featurename = this.data('featurename');
+            this._identity = $("<canvas class='identity' width='60' height='60'></canvas>")
+			this.append(this._identity);				
+			var canvas = this._identity.get(0);
+			if (canvas.getContext){
+				var ctx = canvas.getContext('2d');
+				ctx.beginPath();
+				ctx.font = "6pt Arial";
+				ctx.textBaseline = "top"
+				ctx.fillText(devicename, 15, 5);
+				ctx.translate(5,60);
+				ctx.rotate(-(Math.PI/2));
+				ctx.fillText(featurename, 0, 0);  
+			}
+        },
+        hasSize: function(width, height) {
+            var id = this.data('widgetid');
+            if (id) {
+                var woptions = get_widgets_options(id)
+                return (width == woptions.width && height == woptions.height);                
+            } else {
+                return false;
+            }
+        },
+        deletable: function() {
+            var self = this;
+            this.addClass('deletable')
+                .append('<button><span class=\'offscreen\'>Effacer</span></button>')
+                .find('button').click(function(){
+                        var association = self.data('associationid');
+                        if (association) {
+                            rest.get(['base', 'feature_association', 'del', 'id', association],
+                                function(data) {
+                                    var status = (data.status).toLowerCase();
+                                    if (status == 'ok') {
+                                        rest.get(['base', 'ui_config', 'del', 'by-reference', 'association', association],
+                                            function(data) {
+                                                var status = (data.status).toLowerCase();
+                                                if (status == 'ok') {
+                                                    self.remove();                    
+                                                } else {
+                                                    $.notification('error', data.description);                                          
+                                                }
+                                            }
+                                        );
+                                    } else {
+                                        $.notification('error', data.description);                                          
+                                    }
+                                }
+                            );
+                        }
+                });
+        }
+	});
+    
     /* Mini Widget */
     $.ui.widget.subclass("ui.widget_models", {
         // default options
@@ -71,48 +143,17 @@ $(function(){
                     widget.append(name);
                     var model = $("<div></div>");
                     model.widget_model({
-                        id: id,
+                        widgetid: id,
                         featureid: o.featureid,
-                        featuremodel: o.featuremodel,
+//                        featuremodel: o.featuremodel,
                         featurename: o.featurename,
-                        devicename: o.devicename
-                    });
-                    model.draggable({
-                        helper: "clone",
-                        revert: 'invalid',
-                        appendTo: 'body',
-                        drag: function(event, ui) {
-                            $("#panel").hide();
-                            var association = $(this).attr('id');
-                            if (association) {
-                                rest.get(['base', 'feature_association', 'del', association],
-                                    function(data) {
-                                        var status = (data.status).toLowerCase();
-                                        if (status == 'ok') {
-                                            rest.get(['base', 'ui_config', 'del', 'by-reference', 'association', association],
-                                                function(data) {
-                                                    var status = (data.status).toLowerCase();
-                                                    if (status == 'ok') {
-                                                    } else {
-                                                        $.notification('error', data.description);                                          
-                                                    }
-                                                }
-                                            );
-                                        } else {
-                                            $.notification('error', data.description);                                          
-                                        }
-                                    }
-                                );
-                            }
-                        },
-                        stop: function(event, ui) {
-                            $("#panel").show();
-                            self._init();
-                            if($(this).hasClass("success")) {
-                                $(this).removeClass("success");
-                            } else {
-                                $(this).remove();			
-                            }
+                        devicename: o.devicename,
+                        draggable: {
+                            helper: "clone",
+                            revert: 'invalid',
+                            appendTo: 'body',
+                            drag: ondrag,
+                            stop: onstop
                         }
                     });
                     widget.append(model);
@@ -129,50 +170,43 @@ $(function(){
     $.ui.widget.subclass("ui.widget_model", {
         // default options
         options: {
+            deletable: false
         },
 
         _init: function() {
             var self = this, o = this.options;
-            var woptions = get_widgets_options(o.id)
+            var woptions = get_widgets_options(o.widgetid)
             if (woptions) {
                 o = $.extend ({}, woptions, o);
             }
 
-            this.element.addClass('model')
-                .attr('role', 'listitem')
-			    .attr("widgetid", o.id)
-				.addClass('size' + o.width + 'x' + o.height)
-                .attr("tabindex", 0)
-	            .attr('featureid', o.featureid)
-                .text(o.width + 'x' + o.height);
-			this._identity = $("<canvas class='identity' width='60' height='60'></canvas>")
-			this.element.append(this._identity);				
-			var canvas = this._identity.get(0);
-			if (canvas.getContext){
-				var ctx = canvas.getContext('2d');
-				ctx.beginPath();
-				ctx.font = "6pt Arial";
-				ctx.textBaseline = "top"
-				ctx.fillText(o.devicename, 15, 5);
-				ctx.translate(5,60);
-				ctx.rotate(-(Math.PI/2));
-				ctx.fillText(o.featurename, 0, 0);  
-			}
-        },
-
-        hasSize: function(width, height) {
-            var woptions = get_widgets_options(this.options.id)
-            return (width == woptions.width && height == woptions.height);
+            this.element.addClass('model');
+            this.element.removeAttr('style');
+            this.element.attr('role', 'listitem');
+			this.element.addClass('size' + o.width + 'x' + o.height);
+            this.element.attr("tabindex", 0);
+            this.element.text(o.width + 'x' + o.height);
+            this.element.data({
+                'devicename':o.devicename,
+                'featurename':o.featurename,
+                'featureid':o.featureid,
+                'widgetid': o.widgetid,
+                'associationid': o.associationid
+            });
+            this.element.displayName();
+            this.element.draggable(o.draggable);
+            if (o.deletable) this.element.deletable();
         }
     });
     
     $.extend({
         addAssociation: function(model, zone) {
-            var page_type = zone.attr('page_type');
-            var page_id = zone.attr('page_id');
-            var widget_id = model.attr('widgetid');
-            var place_id = zone.attr('place');
-            rest.get(['base', 'feature_association', 'add', 'feature_id', model.attr('featureid'), 'association_type', page_type, 'association_id', page_id],
+            var page_type = zone.data('page_type');
+            var page_id = zone.data('page_id');
+            var place_id = zone.data('place');
+            var widget_id = model.data('widgetid');
+            var feature_id = model.data('featureid');
+            rest.get(['base', 'feature_association', 'add', 'feature_id', feature_id, 'association_type', page_type, 'association_id', page_id],
                 function(data) {
                     var status = (data.status).toLowerCase();
                     if (status == 'ok') {
@@ -185,6 +219,7 @@ $(function(){
                                         function(data) {
                                             var status = (data.status).toLowerCase();
                                             if (status == 'ok') {
+                                                model.data('associationid', id);
                                             } else {
                                                 $.notification('error', data.description);                                          
                                             }
@@ -207,31 +242,29 @@ $(function(){
                     activeClass: 'state-active',
                     hoverClass: 'state-hover',
                     accept: function(draggable) {
-                        return (($(".model", this).length  < 1) && draggable.widget_model('hasSize', 1, 1));
+                        return (($(".model", this).length  < 1) && draggable.hasSize(1, 1));
                     },
                     drop: ondrop
-            }).attr('place', 'maininfo')
-                .attr('page_type', page_type)
-                .attr('page_id', page_id);
+            })
+                .data({'place':'maininfo','page_type': page_type,'page_id':page_id});
+
             
             $('.mainactions').droppable({
                     activeClass: 'state-active',
                     hoverClass: 'state-hover',
                     accept: function(draggable) {
-                        return (($(".model", this).length  < 4) && draggable.widget_model('hasSize', 1, 1));
+                        return (($(".model", this).length  < 4) && draggable.hasSize(1, 1));
                     },
                     drop: ondrop
-            }).attr('place', 'mainactions')
-                .attr('page_type', page_type)
-                .attr('page_id', page_id);
+            })
+                .data({'place':'mainactions','page_type': page_type,'page_id':page_id});
             
             $('.otheractions').droppable({
                     activeClass: 'state-active',
                     hoverClass: 'state-hover',
                     drop: ondrop
-            }).attr('place', 'otheractions')
-                .attr('page_type', page_type)
-                .attr('page_id', page_id);
+            })
+                .data({'place':'otheractions','page_type': page_type,'page_id':page_id});
     
             var options = null;
             if (page_type == 'house') {
@@ -259,51 +292,20 @@ $(function(){
                                                 var status = (data.status).toLowerCase();
                                                 if (status == 'ok') {
                                                     var model = $("<div id='" + association.id + "' role='listitem'>1x1</div>");
-                                                    model.attr('association', association.id);
                                                     model.widget_model({
-                                                        id: widget,
+                                                        widgetid: widget,
                                                         featureid: association.device_feature_id,
-                                                        featuremodel: data.feature[0].device_feature_model.id,
+//                                                            featuremodel: data.feature[0].device_feature_model.id,
                                                         featurename: data.feature[0].device_feature_model.name,
-                                                        devicename: data.feature[0].device.name
-                                                    })
-                                                    .draggable({
-                                                        helper: false,
-                                                        revert: false,
-                                                        appendTo: 'body',
-                                                        drag: function(event, ui) {
-                                                            $("#panel").hide();
-                                                            var association = $(this).attr('id');
-                                                            if (association) {
-                                                                rest.get(['base', 'feature_association', 'del', 'id', association],
-                                                                    function(data) {
-                                                                        var status = (data.status).toLowerCase();
-                                                                        if (status == 'ok') {
-                                                                            rest.get(['base', 'ui_config', 'del', 'by-reference', 'association', association],
-                                                                                function(data) {
-                                                                                    var status = (data.status).toLowerCase();
-                                                                                    if (status == 'ok') {
-                                                                                    } else {
-                                                                                        $.notification('error', data.description);                                          
-                                                                                    }
-                                                                                }
-                                                                            );
-                                                                        } else {
-                                                                            $.notification('error', data.description);                                          
-                                                                        }
-                                                                    }
-                                                                );
-                                                            }
+                                                        devicename: data.feature[0].device.name,
+                                                        associationid: association.id,
+                                                        draggable: {
+                                                            helper: false,
+                                                            revert: 'invalid',
+                                                            drag: ondrag,
+                                                            stop: onstop
                                                         },
-                                                        stop: function(event, ui) {
-                                                            $("#panel").show();
-                                                            $("#widgetslist").widget_models('update');
-                                                            if($(this).hasClass("success")) {
-                                                                $(this).removeClass("success");
-                                                            } else {
-                                                                $(this).remove();			
-                                                            }
-                                                        }
+                                                        deletable: true
                                                     });
                                                     $("." + place).append(model);
                                                 } else {
@@ -324,4 +326,18 @@ $(function(){
             );
         }
     });
+    
+    function matchFilter(filters, id) {
+        var res = false;
+        if (filters) {
+            $.each(filters, function(index, filter){
+                var afilter = filter.split('.');
+                var aid = id.split('.');
+                res =  (afilter[0] == aid[0] || afilter[0] == '*') && (afilter[1] == aid[1] || afilter[1] == '*') && (afilter[2] == aid[2] || afilter[2] == '*');
+            });
+        } else {
+            res = true;
+        }
+        return res;
+    }
 })(jQuery);
