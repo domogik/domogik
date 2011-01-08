efbcbec02f54cbd3919548fd9257734e98b717a2
Mika64 <devnull@localhost>
2011-01-08 15:58:56 +0100

    zwave : add bin Zwave plugin

--- src/domogik/xpl/helpers/zwave.py
+++ src/domogik/xpl/helpers/zwave.py
@@ -1,4 +1,41 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+""" This file is part of B{Domogik} project (U{http://www.domogik.org}$
+
+License
+=======
+
+B{Domogik} is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+B{Domogik} is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Domogik. If not, see U{http://www.gnu.org/licenses}.
+
+Plugin purpose
+==============
+
+Support Z-wave technology
+
+Implements
+==========
+
+-Zwave
+
+@author: Mika64 <ricart.michael@gmail.com>
+@copyright: (C) 2007-2009 Domogik project
+@license: GPL(v3)
+@organization: Domogik
+"""
+
+
 from domogik.xpl.common.helper import Helper
 from domogik.xpl.common.helper import HelperError
 from domogik.common import logger
--- src/domogik/xpl/lib/zwave.py
+++ src/domogik/xpl/lib/zwave.py
@@ -1,10 +1,58 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+""" This file is part of B{Domogik} project (U{http://www.domogik.org}$
+
+License
+=======
+
+B{Domogik} is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+B{Domogik} is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Domogik. If not, see U{http://www.gnu.org/licenses}.
+
+Plugin purpose
+==============
+
+Support Z-wave technology
+
+Implements
+==========
+
+-Zwave
+
+@author: Mika64 <ricart.michael@gmail.com>
+@copyright: (C) 2007-2009 Domogik project
+@license: GPL(v3)
+@organization: Domogik
+"""
+
+
 import binascii
 import threading
 import serial
 from time import sleep
+
+class ZwaveException(Exception):
+    '''
+    Zwave Exception
+    '''
+            
+    def __init__(self, value):
+        Exception.__init__(self)
+        self.value = value
+                                
+    def __str__(self):
+        return repr(self.value)
+                                            
 
 class zwave(threading.Thread):
     """
