16ccc5be440171cc6df015da4e50cb9de3cacbcc
Fritz <devnull@localhost>
2011-07-28 14:56:43 +0200

    Add uninstall.sh script

---
+++ uninstall.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+#This file is part of B{Domogik} project (U{http://www.domogik.org}).
+#
+#License
+#=======
+#
+#B{Domogik} is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#B{Domogik} is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with Domogik. If not, see U{http://www.gnu.org/licenses}.
+#
+#Module purpose
+#==============
+#
+#Clean domogik installation
+#
+#Implements
+#==========
+#
+#
+#@author: Fritz < fritz.smh@gmail.com>
+#@copyright: (C) 2007-2009 Domogik project
+#@license: GPL(v3)
+#@organization: Domogik
+
+
+# IS root user ?
+if [ $UID -ne 0 ];then
+    echo "Please restart this script as root!"
+    exit 10
+fi
+
+# Ask for confirmation
+echo "This script will uninstall completely Domogik :"
+echo "- Domogik core"
+echo "- Configuration"
+echo "- Plugins"
+echo "- ..."
+echo "Are you sure ? [y/N]"
+read choice
+if [ "x"$choice == "x" ] ; then
+    choice=n
+fi
+if [ $choice != "y" -a $choice != "Y" ] ; then
+    echo "Aborting..."
+    exit 0
+fi
+
+[ ! -f /etc/default/domogik ] && [ ! -f /etc/conf.d/domogik ] && echo "File /etc/default/domogik or /etc/conf.d/domogik doesn't exists : exiting" && exit 1
+
+[ -f /etc/conf.d/domogik ] && . /etc/conf.d/domogik && GLOBAL_CONFIG=/etc/conf.d/domogik
+[ -f /etc/default/domogik ] && . /etc/default/domogik && GLOBAL_CONFIG=/etc/default/domogik
+
+echo "Domogik installation found for user : $DOMOGIK_USER"
+
+#RM="ls -l "  # for simulation
+RM="rm -Rf "
+
+echo "Delete /usr/local/share/domogik"
+$RM /usr/local/share/domogik
+
+CONFIG_FOLDER=/home/$DOMOGIK_USER/.domogik/
+echo "Delete config folder : $CONFIG_FOLDER"
+$RM $CONFIG_FOLDER
+
+echo "Delete $GLOBAL_CONFIG"
+$RM $GLOBAL_CONFIG
+
+for fic in dmgenplug dmgdisplug dmg_manager dmg_send dmg_django
+  do
+    TO_DEL=$(which $fic)
+    echo "Delete $TO_DEL"
+    $RM $TO_DEL
+done
+
+PY_FOLDER=$(dirname $(python -c "print __import__('domogik').__path__[0]"))
+if [ ${PY_FOLDER:0:5} == "/usr/" ] ; then
+    echo "Remove python part : $PY_FOLDER"
+    $RM $PY_FOLDER
+else
+    echo "Not removing $PY_FOLDER"
+    echo "It seems to be development files"
+fi
+
+echo "Notice : database was not suppressed : you must do it manually"
+
+echo "Uninstall complete!"
