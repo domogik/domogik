b4462fd45b2f3541572091fdef15d59f064541c8
mirelsol <marc@mirelsol.org>
2011-12-27 10:16:14 +0100

    Close #1110

--- install/upgrade_repository/versions/001_0_1_0_to_0_2_0-Alter_tables.py
+++ install/upgrade_repository/versions/001_0_1_0_to_0_2_0-Alter_tables.py
@@ -11,6 +11,11 @@ def upgrade(migrate_engine):
     core_plugin_config = Table('core_plugin_config', meta, autoload=True)
     core_plugin_config.c.name.alter(name='id')
 
+    #1110
+    core_device_type = Table('core_device_type', meta, autoload=True)
+    core_device_type.c.name.alter(type=String(80))
+    
+
 def downgrade(migrate_engine):
     meta = MetaData(bind=migrate_engine)
 
@@ -21,3 +26,7 @@ def downgrade(migrate_engine):
     #1064
     core_plugin_config = Table('core_plugin_config', meta, autoload=True)
     core_plugin_config.c.id.alter(name='name')
+
+    #1110
+    core_device_type = Table('core_device_type', meta, autoload=True)
+    core_device_type.c.name.alter(type=String(30))
