5c99dfa7c9307068f9b3dd33cbf9e88e4bad43f4
BOLLINI Cedric <devnull@localhost>
2012-01-22 18:32:10 +0100

    Add karotz plugin and moidf zibase plugin

---
+++ src/domogik/xpl/bin/karotz.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+""" This file is part of B{Domogik} project (U{http://www.domogik.org}).
+
+License
+=======
+
+B{Domogik} is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+B{Domogik} is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Domogik. If not, see U{http://www.gnu.org/licenses}.
+
+Plugin purpose
+==============
+
+Karotz management
+
+Implements
+==========
+
+- KarotzMain
+
+@author: Cedric BOLLINI <cb.dev@sfr.fr>
+@copyright: (C) 2007-2012 Domogik project
+@license: GPL(v3)
+"""
+
+from domogik.xpl.common.xplconnector import Listener, XplTimer
+from domogik.xpl.common.plugin import XplPlugin
+from domogik.xpl.common.xplmessage import XplMessage
+from domogik.xpl.common.queryconfig import Query
+from domogik.xpl.lib.karotz import Karotz
+import traceback
+
+
+class KarotzMain(XplPlugin):
+    '''Manage ZiBase
+    '''	
+    def __init__(self):
+        """ Create lister and launch bg listening
+        """
+        try:
+            XplPlugin.__init__(self, name = 'karotz')
+        except:
+            self.log.error("Error to create Karotz Xplplugin=%s" % (traceback.format_exc()))
+            return
+            
+        self._config = Query(self.myxpl, self.log)
+
+        self.instid = self._config.query('karotz', 'installid')
+        self.lang = self._config.query('karotz', 'language')
+        
+        self.log.info("Creating listener for Karotz")
+        Listener(self.karotz_command, self.myxpl, {'schema': 'karotz.basic',
+                'xpltype': 'xpl-cmnd'})
+        
+        
+        try:
+            self.kar=Karotz(self.log,self.instid)
+        except:
+            self.log.error("Error to create Karotz object=%s" % (traceback.format_exc()))
+            return
+
+        #self.add_stop_cb(self.stop)
+        self.enable_hbeat()
+
+        self.log.info("Plugin ready :)")
+        
+    def karotz_command(self, message):
+        """ Call karotz lib function in function of given xpl message
+            @param message : xpl message
+        """
+        cmd = None
+        dev = None
+        value = None
+        
+        if 'command' in message.data:
+            cmd = message.data['command']
+        if 'device' in message.data:
+            dev = message.data['device']
+        if 'value' in message.data:
+            value=message.data['value']
+
+        self.log.debug(message.data)
+        if value == None :
+            self.log.warning("value not specified")
+            return
+        else:
+            self.log.debug("%s received : device = %s value=%s " % (cmd, dev, value))
+            try:
+                self.log.debug("language=%s",self.lang)
+                self.kar.tts(value,self.lang.upper())
+            except:
+                self.log.error("Error to send command=%s" % (traceback.format_exc()))
+  
+
+if __name__ == "__main__":
+    KarotzMain()
