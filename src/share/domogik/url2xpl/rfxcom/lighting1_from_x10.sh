3ca44271b8ff6f1f3adafc98dc3feeab368d27b3
Fritz <devnull@localhost>
2012-02-27 17:09:26 +0100

    rfxcom : add lighting1_* url2xpl files

---
+++ src/share/domogik/url2xpl/rfxcom/lighting1_from_x10.sh
@@ -0,0 +1,8 @@
+for PROTO in arc elro waveman chacon impuls
+  do
+    for fic in lighting1_x10_bright.xml  lighting1_x10_off.xml lighting1_x10_dim.xml     lighting1_x10_on.xml
+      do
+        newFic=$(echo $fic | sed "s/x10/$PROTO/")
+        sed  "s/x10/arc/g" $fic > $newFic
+      done
+  done
