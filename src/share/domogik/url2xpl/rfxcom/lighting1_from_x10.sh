97e2fb96f6390fa12149a2cc30cc2ac270a04c87
Fritz <devnull@localhost>
2012-03-01 09:14:17 +0100

    rfxcom : lighting3 xml files

--- src/share/domogik/url2xpl/rfxcom/lighting1_from_x10.sh
+++ src/share/domogik/url2xpl/rfxcom/lighting1_from_x10.sh
@@ -3,6 +3,7 @@ for PROTO in arc elro waveman chacon impuls
     for fic in lighting1_x10_bright.xml  lighting1_x10_off.xml lighting1_x10_dim.xml     lighting1_x10_on.xml
       do
         newFic=$(echo $fic | sed "s/x10/$PROTO/")
-        sed  "s/x10/$PROTO/g" $fic > $newFic
+        sed  "s/1_x10/1_$PROTO/g" $fic > $newFic
+        sed  "s/\"x10\"/\"$PROTO\"/g" $fic > $newFic
       done
   done
