9cc178b37eae9247dab2b7e713f195a9e4a17572
vfritschy <devnull@localhost>
2012-03-27 15:18:36 +0200

    Delete drm-yii

--- src/tools/drm-yii/domogikpkg/protected/yiic.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// change the following paths if necessary
-$yiic=dirname(__FILE__).'/../../yii-1.1.7.r3135/framework/yiic.php';
-$config=dirname(__FILE__).'/config/console.php';
-
-require_once($yiic);
