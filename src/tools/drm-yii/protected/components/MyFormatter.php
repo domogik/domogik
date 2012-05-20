24784351d49e67e09c61176cfa45839b97f6e3c2
Ferllings <cedric@domogik.org>
2012-05-20 20:48:43 +0200

    Updated User Doc

--- src/tools/drm-yii/protected/components/MyFormatter.php
+++
@@ -1,8 +0,0 @@
-<?php
-class MyFormatter extends CFormatter {
-    
-    public function formatBooleanIcon($value) {
-        return $value ? CHtml::tag('div', array('class'=>'icon16 icon16-true'), CHtml::tag('span', array('class'=>'offscreen'), "Yes")) : CHtml::tag('div', array('class'=>'icon16 icon16-false'), CHtml::tag('span', array('class'=>'offscreen'), "No"));
-    }    
-}
-?>
