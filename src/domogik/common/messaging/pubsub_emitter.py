a01d2d74324ce26bb91c43b5f54ca3baffab1295
mirelsol <marc@mirelsol.org>
2012-10-25 20:07:13 +0200

    Initial structure with basic pub-sub example

--- src/domogik/common/messaging/pubsub_emitter.py
+++ src/domogik/common/messaging/pubsub_emitter.py
@@ -7,18 +7,22 @@ import zmq
 from random import choice
 from time import sleep
 
+MSG_VERSION = "0_1"
+
 print("PUB-SUB emitter")
 context = zmq.Context()
 socket = context.socket(zmq.PUB)
 socket.connect("tcp://localhost:5559")
 
-countries = ['France', 'Germany', 'Italy', 'Spain']
-events = ['Yellow card', 'Red card', 'Foul', 'Goal']
+categories = {
+    'package' : ['installed', 'uninstalled'],
+    'plugin' : ['enabled', 'disabled'],
+}
 
 while True:
-    topic = choice(countries)
-    data = choice(events)
-    message = "%s : %s" %(topic, data)
+    category = choice(categories.keys())
+    action = choice(categories[category])
+    message = "%s.%s.%s" %(category, action, MSG_VERSION)
     print("Sending message : %s" % message)
     socket.send(message)
     sleep(1)
