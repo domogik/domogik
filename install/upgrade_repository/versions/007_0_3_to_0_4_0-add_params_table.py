c9db087ef2fcd5adbf66a89dcc16a1b290ab0ea0
Maikel Punie <devnull@localhost>
2012-11-07 12:17:40 +0100

    readded the script

---
+++ install/upgrade_repository/versions/007_0_3_to_0_4_0-add_params_table.py
@@ -0,0 +1,32 @@
+from sqlalchemy import *
+from migrate import *
+from domogik.common.sql_schema import XplStat, XplStatParam, XplCommand, XplCommandParam
+
+
+def upgrade(migrate_engine):
+    # bind the engine
+    meta = MetaData(bind=migrate_engine)
+
+    # create the new table
+    table = XplStat.__table__
+    table.create(bind=migrate_engine)
+    table = XplStatParam.__table__
+    table.create(bind=migrate_engine)
+    table = XplCommand.__table__
+    table.create(bind=migrate_engine)
+    table = XplCommandParam.__table__
+    table.create(bind=migrate_engine)
+
+def downgrade(migrate_engine):
+    # bind the engine
+    meta = MetaData(bind=migrate_engine)
+
+    # drop the page table
+    table = XplStat.__table__
+    table.drop()
+    table = XplStatParam.__table__
+    table.drop()
+    table = XplCommand.__table__
+    table.drop()
+    table = XplCommandParam.__table__
+    table.drop()
