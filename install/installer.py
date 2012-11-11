ffa8f242b0ccfb16c95b6ebeab968e43a4c29505
Maikel Punie <devnull@localhost>
2012-11-11 10:08:51 +0100

    forget to remove one page reference

--- install/installer.py
+++ install/installer.py
@@ -170,8 +170,6 @@ def add_initial_data():
     _db.add_device_usage(du_id='scene', du_name='Scene', du_description='Special for Scene plugin',
                          du_default_options='{&quot;actuator&quot;: { &quot;binary&quot;: {&quot;state0&quot;:&quot;False&quot;, &quot;state1&quot;:&quot;True&quot;}, &quot;range&quot;: {&quot;step&quot;:10, &quot;unit&quot;:&quot;%&quot;}, &quot;trigger&quot;: {}, &quot;number&quot;: {} }, &quot;sensor&quot;: {&quot;boolean&quot;: {}, &quot;number&quot;: {}, &quot;string&quot;: {} } }')
 
-    _db.add_page(name='ROOT', parentId=None, descr='', icon='')
-
     # Set sqlalchemy migrate version to the latest one
     rep_v = get_repository_version()
     meta = MetaData(bind=_engine)
