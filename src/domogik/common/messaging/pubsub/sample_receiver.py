159f8ff796e453b17d64d225f9f7a59ab9535564
mirelsol <marc@mirelsol.org>
2012-11-20 20:01:41 +0100

    Cleanup

--- src/domogik/common/messaging/pubsub/sample_receiver.py
+++ src/domogik/common/messaging/pubsub/sample_receiver.py
@@ -8,28 +8,11 @@ from messaging_event import MessagingEventSub
 
 def main(category_filter, action_filter):
     print("PUB-SUB receiver")
-    '''
-    context = zmq.Context()
-    socket = context.socket(zmq.SUB)
-    socket.connect("tcp://localhost:5560")
-
-    socket.setsockopt(zmq.SUBSCRIBE, topic_filter)
-    '''
-
     sub_event = MessagingEventSub(category_filter, action_filter)
 
     while True:
         msg = sub_event.wait_for_event()
         print(msg)
-        '''
-        print("Message id : %s" % message_id)
-        more = socket.getsockopt(zmq.RCVMORE)
-        if more:
-            message_content = socket.recv(zmq.RCVMORE)
-            print("Message content : %s" % message_content)
-        else:
-            print("nothing more")
-        '''
 
 if __name__ == "__main__":
     category_filter = None
