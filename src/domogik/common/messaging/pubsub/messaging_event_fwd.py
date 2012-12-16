11fae317be42cb6f4a62239c94f1576a5aef4f3c
mirelsol <marc@mirelsol.org>
2012-12-16 17:12:01 +0100

    Add license

--- src/domogik/common/messaging/pubsub/messaging_event_fwd.py
+++ src/domogik/common/messaging/pubsub/messaging_event_fwd.py
@@ -1,7 +1,38 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# PUB-SUB forwarder
+""" This file is part of B{Domogik} project (U{http://www.domogik.org}).
+
+License
+=======
+
+B{Domogik} is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+B{Domogik} is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Domogik. If not, see U{http://www.gnu.org/licenses}.
+
+Module purpose
+==============
+
+PUB-SUB forwarder
+
+Implements
+==========
+
+
+@author: Marc SCHNEIDER <marc@mirelsol.org>
+@copyright: (C) 2007-2012 Domogik project
+@license: GPL(v3)
+@organization: Domogik
+"""
 
 import zmq
 from domogik.common.configloader import Loader
@@ -26,8 +57,6 @@ def main():
         
         print("Forwarding messages...")
         zmq.device(zmq.FORWARDER, frontend, backend)
-        
-
     except Exception, e:
         print(e)
         print("Bringing down ZMQ device")
@@ -38,5 +67,4 @@ def main():
         context.term()
 
 if __name__ == "__main__":
-    print("PUB-SUB forwarder")
     main()
--- src/domogik/common/messaging/pubsub/messaging_event_utils.py
+++ src/domogik/common/messaging/pubsub/messaging_event_utils.py
@@ -1,7 +1,38 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Utility class for publish / subscribe pattern
+""" This file is part of B{Domogik} project (U{http://www.domogik.org}).
+
+License
+=======
+
+B{Domogik} is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+B{Domogik} is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Domogik. If not, see U{http://www.gnu.org/licenses}.
+
+Module purpose
+==============
+
+Utility class for publish / subscribe pattern
+
+Implements
+==========
+
+
+@author: Marc SCHNEIDER <marc@mirelsol.org>
+@copyright: (C) 2007-2012 Domogik project
+@license: GPL(v3)
+@organization: Domogik
+"""
 
 import json
 import zmq
