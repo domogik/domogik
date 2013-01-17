dcea4738629db20c565c529cb9945ef42ccf5550
Maikel Punie <devnull@localhost>
2013-01-17 14:52:04 +0100

    example of rest implementation

--- src/domogik/xpl/lib/rest/url.py
+++ src/domogik/xpl/lib/rest/url.py
@@ -1,4 +1,4 @@
-from flask import Flask, g, Response
+from flask import Flask, g, Response, request
 from flask_principal import Principal, Permission, RoleNeed
 from flask.ext.login import LoginManager, login_user, logout_user, \
      login_required, current_user
@@ -31,14 +31,16 @@ def db_helper(action_func):
 def json_response(action_func):
     def create_json_response(*args, **kwargs):
         ret = action_func(*args, **kwargs)
-        code = 200
-        if len(ret) == 2:
-            code = ret[0]
-            resp = ret[1]
+        if type(ret) is list or type(ret) is tuple:
+            if len(ret) == 2:
+                code = ret[0]
+                resp = ret[1]
+            else:
+                code = 200
+                resp = ret[1]
         else:
-            resp = ret
-	print ret
-        print resp
+            code = 204
+            resp = None
         return Response(
             response=json.dumps(resp, cls=domogik_encoder(), check_circular=False),
             status=code,
@@ -47,16 +49,20 @@ def json_response(action_func):
     return create_json_response
 
 # view class registration
-def register_api(view, endpoint, url, pk='id', pk_type='int'):
+def register_api(view, endpoint, url, pk='id', pk_type=None):
     view_func = view.as_view(endpoint)
-    urlHandler.add_url_rule(url, defaults={pk: None},
-                     view_func=view_func, methods=['GET',])
-    urlHandler.add_url_rule(url, view_func=view_func, methods=['POST',])
-    urlHandler.add_url_rule('%s<%s:%s>' % (url, pk_type, pk), view_func=view_func,
+    urlHandler.add_url_rule(url, defaults={pk: None}, view_func=view_func, methods=['GET'])
+    urlHandler.add_url_rule(url, view_func=view_func, methods=['POST'])
+    if pk_type != None:
+        urlHandler.add_url_rule('%s<%s:%s>' % (url, pk_type, pk), view_func=view_func,
                      methods=['GET', 'PUT', 'DELETE'])
-
+    else:
+        urlHandler.add_url_rule('%s<%s>' % (url, pk), view_func=view_func,
+                     methods=['GET', 'PUT', 'DELETE'])
 
 # import the flask urls
 import domogik.xpl.lib.rest.urls.status
 from domogik.xpl.lib.rest.urls.device import deviceAPI
 register_api(deviceAPI, 'device_api', '/device/', pk='device_id', pk_type='int')
+from domogik.xpl.lib.rest.urls.device_usage import deviceUsageAPI
+register_api(deviceUsageAPI, 'device_usage_api', '/device_usage/', pk='usage_id')
