1e5c1484f3276130ea78bf163a027aa71f0c83d8
Maikel Punie <devnull@localhost>
2013-05-22 17:54:58 +0200

    typo fix

--- src/domogik/xpl/lib/rest/stat.py
+++ src/domogik/xpl/lib/rest/stat.py
@@ -183,7 +183,7 @@ class StatsManager:
                                     store = False
                             if store:
                                 # check if we need a conversion
-                                if self._sen.conversion is not None and sel._sen.conversion == '':
+                                if self._sen.conversion is not None and self._sen.conversion == '':
                                     value = call_package_conversion(\
                                                 self._log, self._dev.device_type.plugin_id, \
                                                 sel._sen.conversion, value)
