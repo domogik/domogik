037a85b7e94b5412d32df0e4c79783b0c1c74c2c
Maikel Punie <maikel.punie@gmail.com>
2013-05-29 16:29:55 +0200

    fixed velbus to work again with the new version

--- src/domogik_packages/conversions/velbus.py
+++ src/domogik_packages/conversions/velbus.py
@@ -1,4 +1,12 @@
 class velbusConversions():
     @staticmethod
-    def test(x):
-        print x
+    def from_DT_Switch_to_level(x):
+        if x == '1':
+            return 255
+        else:
+            return 0
+
+    @staticmethod
+    def from_level_to_DT_Scaling(x):
+        # 0 - 255 translated to 0 - 100
+        return round(int(x) / 255 * 100)
