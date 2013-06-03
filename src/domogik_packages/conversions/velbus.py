35cb0eaea3604dbbfb36d675635d9c4355643101
Maikel Punie <maikel.punie@gmail.com>
2013-06-03 19:51:24 +0200

    fixed velbus plugin, added conversions

--- src/domogik_packages/conversions/velbus.py
+++ src/domogik_packages/conversions/velbus.py
@@ -2,9 +2,21 @@ class velbusConversions():
     @staticmethod
     def from_DT_Switch_to_level(x):
         if x == '1':
-            return 255
+            return int(255)
         else:
-            return 0
+            return int(0)
+
+    @staticmethod
+    def from_DT_Scaling_to_level(x):
+        # 0 - 100 translated to 0 - 255
+        return round(int(x) / 100 * 255)
+
+    @staticmethod
+    def from_level_to_DT_Switch(x):
+        if x == '255':
+            return int(1)
+        if x == '0':
+            return int(0)
 
     @staticmethod
     def from_level_to_DT_Scaling(x):
