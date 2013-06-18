ebe6401fc2e0290ba50558a7551461083451517e
Maikel Punie <maikel.punie@gmail.com>
2013-06-18 12:19:19 +0200

    removed the logger from pub/sub

--- pubsub/publisher.py
+++ pubsub/publisher.py
@@ -2,21 +2,17 @@
 import zmq
 import json
 from time import time
-from domogik.common import logger
 from domogik.common.configloader import Loader
 
 MSG_VERSION = "0_1"
 
 class MQPub(object):
     def __init__(self, context, caller_id):
-        log = logger.Logger('{0}_pub'.format(caller_id))
-        self.log = log.get_logger('{0}_pub'.format(caller_id))
         self.caller_id = caller_id
         cfg = Loader('mq').load()
         self.cfg_mq = dict(cfg[1])
         self.s_send = context.socket(zmq.PUB)
         pub_addr= "tcp://{0}:{1}".format(self.cfg_mq['ip'], self.cfg_mq['pub_port'])
-        self.log.debug("Publishing on address : %s" % pub_addr)
         self.s_send.connect(pub_addr)
 
     def __del__(self):
@@ -32,5 +28,4 @@ class MQPub(object):
         msg_id = "%s.%s.%s" %(category, str(time()).replace('.','_'), MSG_VERSION)
         self.s_send.send(msg_id, zmq.SNDMORE)
         self.s_send.send( json.dumps(content) )
-        self.log.debug("%s : id = %s - content = %s" % (self.caller_id, msg_id, content))
 
--- pubsub/subscriber.py
+++ pubsub/subscriber.py
@@ -39,28 +39,22 @@ import zmq
 from zmq.eventloop.zmqstream import ZMQStream
 from zmq.eventloop.ioloop import IOLoop, DelayedCallback
 from time import sleep, time
-from domogik.common import logger
-from domogik.common.configloader import Loader
 from domogik.common.configloader import Loader
 
 MSG_VERSION = "0_1"
 
 class MQSyncSub():
     def __init__(self, context, caller_id, *category_filters):
-        self.log = logger.Logger('mq_sync_sub').get_logger('mq_sync_sub')
         self.s_recv = context.socket(zmq.SUB)
         cfg = Loader('mq').load()
         self.cfg_mq = dict(cfg[1])
         self.caller_id = caller_id
         sub_addr= "tcp://{0}:{1}".format(self.cfg_mq['ip'], self.cfg_mq['sub_port'])
-        self.log.debug("Subscribing to address : %s" % sub_addr)
         self.s_recv.connect(sub_addr)
 
         for category_filter in category_filters:
-            self.log.debug("%s : category filter : %s" % (self.caller_id, category_filter))
             self.s_recv.setsockopt(zmq.SUBSCRIBE, category_filter)
         if len(category_filters) == 0:
-            self.log.debug('No filter')
             self.s_recv.setsockopt(zmq.SUBSCRIBE, '')
     
     def __del__(self):
@@ -77,30 +71,24 @@ class MQSyncSub():
         more = self.s_recv.getsockopt(zmq.RCVMORE)
         if more:
             msg_content = self.s_recv.recv(zmq.RCVMORE)
-            self.log.debug("%s : id = %s - content = %s" % (self.caller_id, msg_id, msg_content))
             return {'id': msg_id, 'content': msg_content}
         else:
             msg_error = "Message not complete (content is missing)!, so leaving..."
-            self.log.error(msg_error)
             raise Exception(msg_error)
 
 class MQAsyncSub():
     def __init__(self, context, caller_id, category_filters):
-        self.log = logger.Logger('mq_async_sub').get_logger('mq_async_sub')
         cfg = Loader('mq').load()
         self.cfg_mq = dict(cfg[1])
         self.caller_id = caller_id
         self.s_recv = context.socket(zmq.SUB)
         sub_addr= "tcp://{0}:{1}".format(self.cfg_mq['ip'], self.cfg_mq['sub_port'])
-        self.log.debug("Subscribing to address : %s" % sub_addr)
         self.s_recv.connect(sub_addr)
 
         if len(category_filters) == 0:
-	    self.log.debug("%s : with no category filter(s)" % (self.caller_id))
 	    self.s_recv.setsockopt(zmq.SUBSCRIBE, '')
         else:
             for category_filter in category_filters:
-	        self.log.debug("%s : category filter : %s" % (self.caller_id, category_filter))
 	        self.s_recv.setsockopt(zmq.SUBSCRIBE, category_filter)
         ioloop = IOLoop.instance()
         self.stream = ZMQStream(self.s_recv, ioloop)
@@ -116,7 +104,6 @@ class MQAsyncSub():
         
         :param: msg = the message received
         """
-        self.log.debug("%s : id = %s - content = %s" % (self.caller_id, msg[0], msg[1]))
         self.on_message(msg[0], json.loads(msg[1]))
   
     def on_message(self, msg_id, content):
