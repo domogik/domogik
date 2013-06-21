38ce0d42656a2b5110bf74a750252aa42379ddec
Maikel Punie <maikel.punie@gmail.com>
2013-06-21 07:42:43 +0200

    added /package/products

--- src/domogik/xpl/lib/rest/urls/package.py
+++ src/domogik/xpl/lib/rest/urls/package.py
@@ -1,30 +1,11 @@
 from domogik.xpl.lib.rest.url import urlHandler, json_response, db_helper
 
-@urlHandler.route('/package/get-mode/')
+@urlHandler.route('/package/products/')
 @json_response
 def pkg_mode():
-    data = {}
-    if urlHandler.rest._package_path == None:
-        data['mode'] = 'development'
-    else:
-        data['mode'] = 'normal'
-    return 200, data
-
-@urlHandler.route('/package/list-repo/')
-@json_response
-def pkg_list_repo():
-    data = []
-    pkg_mgr = PackageManager()
-    for repo in pkg_mgr.get_repositories_list():
-        data.append( {"url" : repo['url'],
-                      "priority" : repo['priority']})
-    return 200, data
-
-
-@urlHandler.route('/package/update-cache/')
-@json_response
-def pkg_update_cache():
-    pkg_mgr = PackageManager()
-    pkg_mgr.update_cache()
-    return 204, ""
-
+    products = []
+    pjson = PackageJson(id)
+    if 'products' in pjson.json:
+        for p in pjson.json['products']:
+            products.append(p)
+    return 200, products
