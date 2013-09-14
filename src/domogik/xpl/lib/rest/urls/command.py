f24c200c07e0818669a435e6653b8f1f23bbe14e
Maikel Punie <maikel.punie@gmail.com>
2013-09-14 18:45:07 +0200

    message sending works, the notification from the plugin is still a todo

--- src/domogik/xpl/lib/rest/urls/command.py
+++ src/domogik/xpl/lib/rest/urls/command.py
@@ -1,10 +1,11 @@
 from domogik.xpl.lib.rest.url import urlHandler, json_response, timeit
+from domogik.xpl.common.xplmessage import XplMessage
 from domogik.common.configloader import Loader
 import sys
 import os
 import domogik
 from subprocess import Popen, PIPE
-from flask import Response
+from flask import Response, request
 
 @urlHandler.route('/cmd/id/<int:cid>', methods=['GET'])
 @json_response
@@ -37,19 +38,19 @@ def api_command(cid):
         msg.add_data({p.key : p.value})
     # dynamic params
     for p in cmd.params:
-        if self.get_parameters(p.key):
-            value = self.get_parameters(p.key)
+        if request.args.get(p.key):
+            value = request.args.get(p.key)
             # chieck if we need a conversion
-            if p.conversion is not None and p.conversion != '':
-                value = call_package_conversion(\
-                            urlHandler.log, dev.device_type.plugin_id, \
-                            p.conversion, value)
+            #if p.conversion is not None and p.conversion != '':
+            #    value = call_package_conversion(\
+            #                urlHandler.log, dev.device_type.plugin_id, \
+            #                p.conversion, value)
             # pluginName.conversion.<proc name>
             msg.add_data({p.key : value})
         else:
             return 400, {msg: "Parameter ({0}) for device command msg is not provided in the url".format(p.key)}
     # send out the msg
-    self.myxpl.send(msg)
+    urlHandler.xpl.send(msg)
     ### Wait for answer
     stat_msg = None
     if xplstat != None:
@@ -64,7 +65,7 @@ def api_command(cid):
         # get xpl message from queue
         try:
             urlHandler.log.debug("Command : wait for answer...")
-            stat_msg = self._get_from_queue(self._queue_command, 'xpl-trig', xplstat.schema, filters)
+            #stat_msg = self._get_from_queue(self._queue_command, 'xpl-trig', xplstat.schema, filters)
         except Empty:
             return 400, {msg: "No data or timeout on getting command response"}
         urlHandler.log.debug("Command : message received : {0}".format(stat_msg))
