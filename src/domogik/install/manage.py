c894f0c069a2a86f0a1748c2db4990a51b7d916c
Maikel Punie <maikel.punie@gmail.com>
2013-10-05 16:33:52 +0200

    db upgrade is now using alembic instead of sql-alchemy migrate

--- src/domogik/install/manage.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-from migrate.versioning.shell import main
-from domogik.common import database
-_db = database.DbHelper()
-main(url=_db.get_url_connection_string(), debug='False', repository='upgrade_repository')
