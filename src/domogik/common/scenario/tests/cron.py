858775dbc83c4602be979328a1769f5674a98c9a
Maikel Punie <maikel.punie@gmail.com>
2013-10-22 09:57:11 +0200

    added cron like test/parameter to scenarios close #1755

---
+++ src/domogik/common/scenario/tests/cron.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+""" This file is part of B{Domogik} project (U{http://www.domogik.org}).
+
+License
+=======
+
+B{Domogik} is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+B{Domogik} is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Domogik. If not, see U{http://www.gnu.org/licenses}.
+
+@author: Maikel Punie <maikel.punie@gmail.com>
+@copyright: (C) 2007-2009 Domogik project
+@license: GPL(v3)
+@organization: Domogik
+"""
+
+from domogik.common.scenario.tests.abstract import AbstractTest
+from time import sleep
+
+class CronTest(AbstractTest):
+    """ Simple test to check if a word is contained in an url
+    """
+
+    def __init__(self, log = None, xpl = None, trigger = None):
+        AbstractTest.__init__(self, log, xpl, trigger)
+        self.set_description("Trigger on a certain timed event")
+        self.add_parameter("cron", "cron.CronParameter")
+
+    def evaluate(self):
+        """ Evaluate if the text appears in the content of the page referenced by url
+        """
+        params = self.get_raw_parameters()
+        crn = params["cron"]
+        print crn
+        if crn.evaluate() == None:
+            return None
+        else:
+            res = crn.evaluate()
+            self._log.debug("Evaluate {0} : {1}".format(crn, res))
+            return res
+
+
+if __name__ == "__main__":
+    import logging
+    TEST = None
+
+    def mytrigger(test):
+        print "Trigger called by test %s, refreshing state" % test
+        st = TEST.evaluate()
+        print "state is %s" % st
+
+    FORMAT = "%(asctime)-15s %(message)s"
+    logging.basicConfig(format=FORMAT)
+    TEST = CronTest(logging, trigger = mytrigger)
+    print TEST
+    print "getting parameters"
+    p = TEST.get_parameters()
+    print p
+    print "===="
+    print "Trying to evaluate : %s" % TEST.evaluate()
+    print "===="
+    print "set data for parameters"
+    data = { "cron": { "cron" : "*/2 * * * *"} }
+    TEST.fill_parameters(data)
+    sleep(5)
+    print "Trying to evaluate : %s" % TEST.evaluate()
+    TEST.destroy()
