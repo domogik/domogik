0d0221a5f45fa500f6d004ba3abb5c9ed1d51b54
fritz-smh <fritz.smh@domogik.org>
2013-11-08 23:17:47 +0100

    travis : update scripts

--- src/domogik/tests/travis/travis-install-plugin.sh
+++ src/domogik/tests/travis/travis-install-plugin.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e 
 # The -e option will make the bash stop if any command raise an error ($? != 0)
 
-ln -s $TRAVIS_BUILD_DIR /var/lib/domogik/domogik_packages/plugin_$1
-ls -l /var/lib/domogik/domogik_packages/plugin_$1/
+ln -s $TRAVIS_BUILD_DIR /var/lib/domogik/domogik_packages/plugin_$DMG_PLUGIN
+ls -l /var/lib/domogik/domogik_packages/plugin_$DMG_PLUGIN/
