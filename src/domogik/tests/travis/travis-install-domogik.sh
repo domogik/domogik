a18a57c29be7badda8c9324b18ee69908b7d11bf
fritz-smh <fritz.smh@domogik.org>
2013-11-08 22:12:53 +0100

    travis : add scripts for install

---
+++ src/domogik/tests/travis/travis-install-domogik.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e 
+# The -e option will make the bash stop if any command raise an error ($? != 0)
+
+sudo mkdir -p /var/lock/domogik
+sudo chown $LOGNAME:root /var/lock/domogik
+sudo touch /var/lock/domogik/config.lock
+sudo chown $LOGNAME:root /var/lock/domogik/config.lock
+sudo python install.py --user $LOGNAME --command-line --domogik_log_level debug --domogik_bind_interface 127.0.0.1 --database_name domogik --database_user travis --rest_interfaces lo --rest_clean_json True --mq_ip 127.0.0.1 --hub_interfaces lo --hub_log_level info --hub_log_bandwidth True --hub_log_invalid_data True
+sudo chown $LOGNAME:root /var/lock/domogik
+sudo chown $LOGNAME:root /var/lock/domogik/config.lock
+sudo chown $LOGNAME:root /var/log/domogik/*
+ls -l /var/lock/domogik/
+
---
+++ src/domogik/tests/travis/travis-setup-database.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e 
+# The -e option will make the bash stop if any command raise an error ($? != 0)
+
+"mysql -e 'create database domogik;'"
+echo "USE mysql;\nUPDATE user SET password=PASSWORD('domopass') WHERE user='travis';\nFLUSH PRIVILEGES;\n" | mysql -u root
+
