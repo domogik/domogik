fc1f00554bbe52fda217f954bad58c89829c8bc1
Maikel Punie <maikel.punie@gmail.com>
2013-11-11 16:46:14 +0100

    added apt-get update before installing packages

--- src/domogik/tests/travis/travis-install-dependencies.sh
+++ src/domogik/tests/travis/travis-install-dependencies.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e 
 # The -e option will make the bash stop if any command raise an error ($? != 0)
 
+sudo apt-get update
 sudo apt-get install libzmq3-dev
 sudo pip install pyzmq
 sudo apt-get install python-argparse
