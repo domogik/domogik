622c986db7020287af1e325bb297714ca9dd9b4a
Maikel Punie <maikel.punie@gmail.com>
2013-11-11 18:20:09 +0100

    bad option fix

--- src/domogik/tests/travis/travis-install-domogik.sh
+++ src/domogik/tests/travis/travis-install-domogik.sh
@@ -6,7 +6,7 @@ sudo chown $LOGNAME:root /var/lock/domogik
 sudo touch /var/lock/domogik/config.lock
 sudo chown $LOGNAME:root /var/lock/domogik/config.lock
 python setup.py install
-sudo python install.py --no-setup True --user $LOGNAME --command-line --domogik_log_level debug --domogik_bind_interface 127.0.0.1 --database_name domogik --database_user travis --rest_interfaces lo --rest_clean_json True --mq_ip 127.0.0.1 --hub_interfaces lo --hub_log_level info --hub_log_bandwidth True --hub_log_invalid_data True
+sudo python install.py --no-setup --user $LOGNAME --command-line --domogik_log_level debug --domogik_bind_interface 127.0.0.1 --database_name domogik --database_user travis --rest_interfaces lo --rest_clean_json True --mq_ip 127.0.0.1 --hub_interfaces lo --hub_log_level info --hub_log_bandwidth True --hub_log_invalid_data True
 sudo chown $LOGNAME:root /var/lock/domogik
 sudo chown $LOGNAME:root /var/lock/domogik/config.lock
 sudo chown $LOGNAME:root /var/log/domogik/*
