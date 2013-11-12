3bcb3d88bb44bbdb49e4e04b5aa8058b0bed9fb7
Maikel Punie <maikel.punie@gmail.com>
2013-11-12 16:19:40 +0100

    removed old plugin files

--- src/share/domogik/todel_url2xpl/rfxcom/lighting2_from_ac.sh
+++
@@ -1,8 +0,0 @@
-for PROTO in homeeasy_eu
-  do
-    for fic in lighting2_ac_on.xml lighting2_ac_off.xml lighting2_ac_preset.xml 
-      do
-        newFic=$(echo $fic | sed "s/ac/$PROTO/")
-        sed  "s/lighting2_ac/lighting2_$PROTO/g" $fic > $newFic
-      done
-  done
