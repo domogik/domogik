218fc1ccbf503eb1b6086ba7babbea5a80e1f91e
Maikel Punie <maikel.punie@gmail.com>
2013-11-18 12:56:49 +0100

    add debug

--- src/domogik/tests/travis/travis-start-domogik.sh
+++ src/domogik/tests/travis/travis-start-domogik.sh
@@ -1,10 +1,7 @@
 #!/bin/bash -e 
 # The -e option will make the bash stop if any command raise an error ($? != 0)
 
-which dmg_hub
-which dmg_broker
-which dmg_forwarder
-echo $PATH
+lsvirtualenv
 sudo /etc/init.d/domogik start
 sleep 10
 cat /var/log/domogik/manager.log
