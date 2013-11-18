7b27f922904ffb797d9855669a4af424530f6ccd
Maikel Punie <maikel.punie@gmail.com>
2013-11-18 13:06:00 +0100

    add debug

--- src/domogik/tests/travis/travis-start-domogik.sh
+++ src/domogik/tests/travis/travis-start-domogik.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -e 
 # The -e option will make the bash stop if any command raise an error ($? != 0)
 
-lsvirtualenv
-sudo /etc/init.d/domogik start
+sudo -s -- 'source /home/travis/virtualenv/python2.7/bin/activate; /etc/init.d/domogik start'
 sleep 10
 cat /var/log/domogik/manager.log
 
