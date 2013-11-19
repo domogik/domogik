79061655d152fedb823e8a55b262b5f7295ba2ed
Maikel Punie <maikel.punie@gmail.com>
2013-11-19 13:04:21 +0100

    fixed running of unit testcases

---
+++ src/domogik/tests/run_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# unittests
+python $TRAVIS_BUILD_DIR/src/domogik/tests/unittests/scenario_parameters.py
+python $TRAVIS_BUILD_DIR/src/domogik/tests/unittests/xplmessage_test.py
+
+# testcases for domogik
+python $TRAVIS_BUILD_DIR/src/domogik/tests/domogik_testcase.py
