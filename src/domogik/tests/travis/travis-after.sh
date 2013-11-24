7314934a36ecce8053527c579ec52e6a06d1561e
fritz-smh <fritz.smh@domogik.org>
2013-11-24 15:20:25 +0100

    travis : improve after script

--- src/domogik/tests/travis/travis-after.sh
+++ src/domogik/tests/travis/travis-after.sh
@@ -15,7 +15,7 @@ ls -l /var/log/domogik/
 for fic in /etc/default/domogik /etc/domogik/domogik.cfg /var/log/domogik/db_api.log /var/log/domogik/dbmgr.log /var/log/domogik/manager.log /var/log/domogik/rest.log /var/log/domogik/xplgw.log /var/log/domogik/mq_broker.log /var/log/xplhub/bandwidth.csv /var/log/xplhub/client_list.txt /var/log/xplhub/invalid_data.csv /var/log/xplhub/xplhub.log
   do
     echo ""
-    echo "********** /etc/default/domogik *******************"
+    echo "********** $fic *******************"
     echo ""
     cat $fic
     echo ""
