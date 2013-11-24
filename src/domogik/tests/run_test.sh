0d6416d3a4d9ba845c413e9b529774680f8bfcb6
fritz-smh <fritz.smh@domogik.org>
2013-11-24 11:59:30 +0100

    update tests

--- src/domogik/tests/run_test.sh
+++ src/domogik/tests/run_test.sh
@@ -8,6 +8,9 @@ echo "ps aux | grep domo"
 ps aux | grep domo
 echo "ps aux | grep dmg"
 ps aux | grep dmg
+echo "ps aux | grep rest"
+echo "ps aux | grep dbmgr"
+echo "ps aux | grep xplgw"
 echo "ps u -U $LOGNAME"
 ps u -U $LOGNAME
 
