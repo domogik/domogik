a8abbc4d20d3e1da74b4dce6a829d9e4c3914678
Maikel Punie <maikel.punie@gmail.com>
2014-07-17 15:24:11 +0200

    fix #1952 /rest url bad in case of different rest port

--- src/domogik/admin/views/rest.py
+++ src/domogik/admin/views/rest.py
@@ -1,11 +1,18 @@
 from domogik.admin.application import app
 from flask import render_template
 from flask.ext.babel import gettext, ngettext
+from domogik.common.configloader import Loader, CONFIG_FILE
 import requests
 
 @app.route('/rest')
 def rest():
-    r = requests.get("http://localhost:40405/map") 
+
+    cfg = Loader('rest')
+    config = cfg.load()
+    conf = dict(config[1])
+    port = conf['port']                          
+
+    r = requests.get("http://localhost:{0}/map".format(port))
 
     return render_template('rest.html',
         mactive="rest",
