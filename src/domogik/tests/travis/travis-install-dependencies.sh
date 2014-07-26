c81a6a74ee830ed3364fd8f253af6a157e04c4ac
pnb optiplex <pn_bouteville@yahoo.fr>
2014-07-26 10:39:08 +0200

    remove dist check for travis, travis is a ubuntu ...

--- src/domogik/tests/travis/travis-install-dependencies.sh
+++ src/domogik/tests/travis/travis-install-dependencies.sh
@@ -3,14 +3,6 @@
 echo "PYTHONXXXX:"
 ls -l $(which python)
 
-OS=$(lsb_release -si)
-ARCH=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
-VER=$(lsb_release -sr)
-
-echo "OS  :$OS"
-echo "ARCH:$ARCH"
-echo "VER :$VER"
-
 sudo apt-get update
 sudo apt-get install libzmq3-dev
 sudo pip install pyzmq
@@ -20,10 +12,5 @@ sudo apt-get install python2.7-dev gcc
 sudo apt-get install libssl-dev
 sudo apt-get install libmysqlclient-dev mysql-client
 #sudo apt-get install python-psycopg2
-if [ "$OS" == "Debian" ];then
-    sudo apt-get install python-mysqldb 
-    sudo apt-get install Magic-file-extensions
-else
-    sudo pip install psycopg2
-fi
+sudo pip install psycopg2
 
