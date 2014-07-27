860af9161d492a8016fc75f52109c91163664c78
tikismoke <webmaster@tikijs.dyndns.org>
2014-07-27 15:33:31 +0200

    add '?' in nbztts URL
    Don't know really why but it seems that the '?' convert to %3F before deed not work correctly.
    
    Place here it correct this problem.

--- src/domogik_packages/xpl/lib/nbz_tts.py
+++ src/domogik_packages/xpl/lib/nbz_tts.py
@@ -71,11 +71,11 @@ class NBZNotification:
             self._log.debug("NOTIF : message " + self._message)
             self._log.debug("NOTIF : voice  " + self._voice)
 
-            ttsurl="http://" + self._server + "&sn=" + self._serial + "&token=" + self._token + "&tts=" + self._message + "&" + self._voice
-            staturl="http://" + self._server + "&sn=" + self._serial + "&token=" + self._token + "&action=7&silent&strip"
-            wakupurl="http://" + self._server + "&sn=" + self._serial + "&token=" + self._token + "&action=13&silent&strip"
-            sleepurl="http://" + self._server + "&sn=" + self._serial + "&token=" + self._token + "&action=14&silent&strip"
-            chorurl="http://" + self._server + "&sn=" + self._serial + "&token=" + self._token + "&chor=10,0,motor,1,20,0,0,0,motor,0,20,0,0,1,led,0,0,238,0"
+            ttsurl="http://" + self._server + "?&sn=" + self._serial + "&token=" + self._token + "&tts=" + self._message + "&" + self._voice
+            staturl="http://" + self._server + "?&sn=" + self._serial + "&token=" + self._token + "&action=7&silent&strip"
+            wakupurl="http://" + self._server + "?&sn=" + self._serial + "&token=" + self._token + "&action=13&silent&strip"
+            sleepurl="http://" + self._server + "?&sn=" + self._serial + "&token=" + self._token + "&action=14&silent&strip"
+            chorurl="http://" + self._server + "?&sn=" + self._serial + "&token=" + self._token + "&chor=10,0,motor,1,20,0,0,0,motor,0,20,0,0,1,led,0,0,238,0"
 
             self._log.debug("call state on nabaztag : " + staturl)
             statreq = urllib2.Request(staturl)
