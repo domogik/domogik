d6d00ec0b500a0da6bb71bc3fd8dfd8f0b2e6133
pnb <pn_bouteville@yahoo.fr>
2014-07-28 23:18:07 +0200

    create database for debian

--- debian_packages_install.sh
+++ debian_packages_install.sh
@@ -35,4 +35,14 @@ pkg_list="\
 
 sudo apt-get install $pkg_list
 
+echo to create data base enter Root password:
+mysql -p -u root << TXT 
+create database domogik;
+grant usage on *.* to domogik@localhost identified by 'domopass';
+grant all privileges on domogik.* to domogik@localhost ;
+quit
+TXT
+if [ "$?" != 0 ];then
+    echo "database already present ?"
+fi
 
