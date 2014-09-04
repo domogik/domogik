015bcf3272d35dfc1dc95f8214f70c8a8f87e758
fritz-smh <fritz.smh@gmail.com>
2014-09-04 22:42:37 +0200

    travis : try to set the default shell for already existing user travis to /bin/bash (2)

--- src/domogik/tests/travis/travis-change-shell.sh
+++ src/domogik/tests/travis/travis-change-shell.sh
@@ -1,2 +1,8 @@
 # change the travis shell to /bin/bash to avoid some errors 
+cat /etc/passwd
+echo "====="
 sudo usermod -s /bin/bash travis
+echo "====="
+ls -l /bin/bash
+echo "====="
+cat /etc/passwd
