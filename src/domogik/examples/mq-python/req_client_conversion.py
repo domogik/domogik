a838550e0719b1bb37859bd282dd46d72295c557
fritz-smh <fritz.smh@gmail.com>
2014-10-02 22:59:20 +0200

    Doc : start to work on the MQ doc with apidoc

--- src/domogik/examples/mq-python/req_client_conversion.py
+++ src/domogik/examples/mq-python/req_client_conversion.py
@@ -1,4 +1,36 @@
 #!/usr/bin/python
+"""
+@api {REQ} client.conversion.get Request to get all the clients conversion functions
+@apiVersion 0.4.0
+@apiName client.conversion.get
+@apiGroup Client
+@apiDescription This request is used to ask Domogik's manager the list of all the conversion functions provided by the plugins
+* Source client : XplGw core component 
+* Target client : always 'manager' 
+
+@apiExample {python} Example usage:
+    cli = MQSyncReq(zmq.Context())
+    msg = MQMessage()
+    msg.set_action('client.conversion.get')
+    print(cli.request('manager', msg.get(), timeout=10).get())
+
+@apiSuccessExample {json} Success-Response:
+[
+    'client.conversion.result',
+    '{
+        "core-scenario.darkstar": null,
+        "core-dbmgr.darkstar": null,
+        "core-xplgw.darkstar": null,
+        "core-rest.darkstar": null,
+        "plugin-diskfree.darkstar": {
+            
+        },
+        "plugin-teleinfo.darkstar": {
+            
+        }
+    }'
+]
+"""
 
 import zmq
 from zmq.eventloop.ioloop import IOLoop
@@ -8,6 +40,6 @@ from domogikmq.message import MQMessage
 cli = MQSyncReq(zmq.Context())
 msg = MQMessage()
 msg.set_action('client.conversion.get')
-print cli.request('manager', msg.get(), timeout=10).get()
+print(cli.request('manager', msg.get(), timeout=10).get())
 
 
