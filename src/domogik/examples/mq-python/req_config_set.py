70aa218bfba062512215dd71d636fbb1f7ff019b
fritz-smh <fritz.smh@gmail.com>
2014-10-05 22:23:31 +0200

    doc : MQ doc with apidoc

--- src/domogik/examples/mq-python/req_config_set.py
+++ src/domogik/examples/mq-python/req_config_set.py
@@ -1,4 +1,42 @@
 #!/usr/bin/python
+"""
+@api {REQ} config.set Set a value for a configuration item of a client
+@apiVersion 0.4.0
+@apiName config.set
+@apiGroup Clients configuration
+@apiDescription This request is used to ask Domogik's dbmgr the value of a configuration item
+* Source client : Domogik admin, any other interface which can manage the clients
+* Target client : always 'dbmgr' 
+
+@apiExample {python} Example usage:
+    cli = MQSyncReq(zmq.Context())
+    msg = MQMessage()
+    msg.set_action('config.set')
+    msg.add_data('type', 'plugin')
+    msg.add_data('host', 'darkstar')
+    msg.add_data('name', 'diskfree')
+    msg.add_data('data', {'interval' : 6})
+    print(cli.request('dbmgr', msg.get(), timeout=10).get())
+    
+@apiParam {String} type The client type
+@apiParam {String} host The host on which is hosted the client
+@apiParam {String} name The client name 
+@apiParam {String} data A dictionnary of the configuration elements to set : {'key1' : 'value1', 'key2' : 'value2', ...}
+
+@apiSuccessExample {json} Success-Response:
+[
+    'config.result',
+    '{
+        "status": true,
+        "name": "diskfree",
+        "reason": "",
+        "value": true,
+        "host": "darkstar",
+        "key": "configured",
+        "type": "plugin"
+    }'
+]
+"""
 
 import zmq
 from zmq.eventloop.ioloop import IOLoop
@@ -12,5 +50,5 @@ msg.add_data('type', 'plugin')
 msg.add_data('host', 'darkstar')
 msg.add_data('name', 'diskfree')
 msg.add_data('data', {'interval' : 6})
-print cli.request('dbmgr', msg.get(), timeout=10).get()
+print(cli.request('dbmgr', msg.get(), timeout=10).get())
 
--- src/domogik/examples/mq-python/req_datatype.py
+++ src/domogik/examples/mq-python/req_datatype.py
@@ -1,4 +1,51 @@
 #!/usr/bin/python
+"""
+@api {REQ} datatype.get Get the list of all defined datatypes
+@apiVersion 0.4.0
+@apiName datatype.get 
+@apiGroup Datatypes
+@apiDescription This request is used to ask Domogik's manager the list of all defined datatypes
+* Source client : Any client that need the datatypes list (user interfaces for example)
+* Target client : always 'manager' 
+
+@apiExample {python} Example usage:
+    cli = MQSyncReq(zmq.Context())
+    msg = MQMessage()
+    msg.set_action('datatype.get')
+    print(cli.request('manager', msg.get(), timeout=10).get())
+    
+@apiSuccessExample {json} Success-Response:
+[
+    'datatype.result',
+    '{
+        "datatypes": {
+            "DT_HVACVent": {
+                "childs": [
+                    
+                ],
+                "values": {
+                    "1": "Heat",
+                    "0": "Auto",
+                    "3": "Fan only",
+                    "2": "Cool",
+                    "4": "Dry"
+                }
+            },
+            "DT_OpenClose": {
+                "labels": {
+                    "1": "Close",
+                    "0": "Open"
+                },
+                "childs": [
+                    
+                ],
+                "parent": "DT_Bool"
+            },
+            ...
+        }
+    }'
+]
+"""
 
 import zmq
 from zmq.eventloop.ioloop import IOLoop
@@ -8,5 +55,5 @@ from domogikmq.message import MQMessage
 cli = MQSyncReq(zmq.Context())
 msg = MQMessage()
 msg.set_action('datatype.get')
-print cli.request('manager', msg.get(), timeout=10).get()
+print(cli.request('manager', msg.get(), timeout=10).get())
 
