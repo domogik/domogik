70a9adeb1a6b781e49dc1d4205e0cb227876aeb6
Maikel Punie <maikel.punie@gmail.com>
2015-01-07 16:25:36 +0100

    travis does not ened to start domogik-mq anymore

--- src/domogik/tests/travis/travis-start-domogik.sh
+++ src/domogik/tests/travis/travis-start-domogik.sh
@@ -3,8 +3,9 @@
 
 source /home/travis/virtualenv/python2.7/bin/activate
 
-/etc/init.d/domogik-mq start
-sleep 10
+# AS of 7/01/2015 this should not be needed anymore
+#/etc/init.d/domogik-mq start
+#sleep 10
 /etc/init.d/domogik start
 sleep 10
 cat /var/log/domogik/manager.log
