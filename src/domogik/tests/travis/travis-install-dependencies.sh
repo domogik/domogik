014772f0f713bee10e6c2c6bd9156868a7486bf6
fritz-smh <fritz.smh@gmail.com>
2015-01-10 15:34:53 +0100

    travis : try to fix a dep (again)

--- src/domogik/tests/travis/travis-install-dependencies.sh
+++ src/domogik/tests/travis/travis-install-dependencies.sh
@@ -9,5 +9,6 @@ sudo apt-get install python2.7-dev gcc
 sudo apt-get install libssl-dev
 sudo apt-get install libmysqlclient-dev mysql-client
 sudo pip install psycopg2
-sudo apt-get install python-versiontools
+#sudo apt-get install python-versiontools
+sudo pip install python-version-info
 
