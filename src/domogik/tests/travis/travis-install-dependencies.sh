2fab17dd3bfb1974ade0fb1ea4d7ef477c3c50d0
Maikel Punie <maikel.punie@gmail.com>
2015-01-17 15:46:47 +0100

    keep pip in the virtualenv

--- src/domogik/tests/travis/travis-install-dependencies.sh
+++ src/domogik/tests/travis/travis-install-dependencies.sh
@@ -8,10 +8,10 @@ pip install argparse
 sudo apt-get install python2.7-dev gcc
 sudo apt-get install libssl-dev
 sudo apt-get install libmysqlclient-dev mysql-client
-sudo pip install psycopg2
+pip install psycopg2
 #sudo apt-get install python-versiontools
 #sudo pip install python-version-info
-sudo pip install docutils
+pip install docutils
 #rm /usr/lib/python2.7/dist-packages/easy_install.pyc
-sudo pip install python-daemon==2.0.2 ## see http://stackoverflow.com/questions/27972349/installing-latest-python-daemon-2-0-3-breaks-subsequent-pip-installs
+pip install python-daemon==2.0.2 ## see http://stackoverflow.com/questions/27972349/installing-latest-python-daemon-2-0-3-breaks-subsequent-pip-installs
 
