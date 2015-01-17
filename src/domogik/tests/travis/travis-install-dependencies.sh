6f8eb2293b40c6bc4de7ea2619b586427ea558e4
Maikel Punie <maikel.punie@gmail.com>
2015-01-17 15:56:03 +0100

    readded ==2.0.2

--- src/domogik/tests/travis/travis-install-dependencies.sh
+++ src/domogik/tests/travis/travis-install-dependencies.sh
@@ -10,4 +10,4 @@ sudo apt-get install libssl-dev
 sudo apt-get install libmysqlclient-dev mysql-client
 pip install psycopg2
 pip install docutils
-pip install python-daemon
+pip install python-daemon==2.0.2
