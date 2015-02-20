5b6d62764d4cf9f7c90de584c705634ef78acbd0
Maikel Punie <maikel.punie@gmail.com>
2015-02-20 13:29:16 +0100

    small fix 3

--- src/domogik/tests/travis/travis-run-test-plugin.sh
+++ src/domogik/tests/travis/travis-run-test-plugin.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -e
+echo "=== Start virtualenv ==="
+source /home/travis/virtualenv/python2.7/bin/activate
+
 echo "=== Install the plugin ==="
-pwd
-echo $TRAVIS_BUILD_DIR
-sudo /usr/local/bin/dmg_package -i https://github.com/domogik/domogik-plugin-test/archive/master.zip
+sudo dmg_package -i https://github.com/domogik/domogik-plugin-test/archive/master.zip
 ls /var/lib/domogik/domogik_packages
 
 echo "=== Run the plugin testcases ==="
 sudo src/domogik/tests/bin/testrunner.py /var/lib/domogik/domogik_packages/plugin_test/tests
 
 echo "=== Uninstall the plugin ==="
-sudo /usr/local/bin/dmg_package -r plugin_test
+sudo dmg_package -r plugin_test
