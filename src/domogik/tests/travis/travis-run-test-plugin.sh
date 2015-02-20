41608b3aec9c1aa297b4186d2d11839775417199
Maikel Punie <maikel.punie@gmail.com>
2015-02-20 13:03:38 +0100

    small fix

--- src/domogik/tests/travis/travis-run-test-plugin.sh
+++ src/domogik/tests/travis/travis-run-test-plugin.sh
@@ -1,9 +1,10 @@
+#!/bin/bash -e
 echo "=== Install the plugin ==="
-sudo dmg_package -i https://github.com/domogik/domogik-plugin-test/archive/master.zip
+sudo /usr/local/bin/dmg_package -i https://github.com/domogik/domogik-plugin-test/archive/master.zip
+ls /var/lib/domogik/domogik_packages
 
 echo "=== Run the plugin testcases ==="
 sudo src/domogik/tests/bin/testrunner.py /var/lib/domogik/domogik_packages/plugin_test/tests
-ls /var/lib/domogik/domogik_packages
 
 echo "=== Uninstall the plugin ==="
-sudo dmg_package -r plugin_test
+sudo /usr/local/bin/dmg_package -r plugin_test
