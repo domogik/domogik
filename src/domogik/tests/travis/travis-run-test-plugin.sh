3f67e2247b85aa9b12648911a12db314a44d18bb
Maikel Punie <maikel.punie@gmail.com>
2015-02-20 15:15:24 +0100

    running testrunner with --allow-alter check reurncode

--- src/domogik/tests/travis/travis-run-test-plugin.sh
+++ src/domogik/tests/travis/travis-run-test-plugin.sh
@@ -6,7 +6,8 @@ echo "=== Install the plugin ==="
 dmg_package -i https://github.com/domogik/domogik-plugin-test/archive/master.zip
 
 echo "=== Run the plugin testcases ==="
-dmg_testrunner /var/lib/domogik/domogik_packages/plugin_test/tests
+dmg_testrunner --allow-alter /var/lib/domogik/domogik_packages/plugin_test/tests
+if [[ $rc != 0 ]]; then exit $rc; fi
 
 echo "=== Uninstall the plugin ==="
 dmg_package -r plugin_test
