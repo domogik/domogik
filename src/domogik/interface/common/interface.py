5f8c2c94357ed9198bd78731b8a3a805a14c4e73
fritz-smh <fritz.smh@gmail.com>
2015-04-21 15:07:00 +0200

    Improve interface

--- src/domogik/interface/common/interface.py
+++ src/domogik/interface/common/interface.py
@@ -22,7 +22,7 @@ along with Domogik. If not, see U{http://www.gnu.org/licenses}.
 Plugin purpose
 ==============
 
-Base class for all xPL clients
+Base class for all interfaces clients
 
 Implements
 ==========
@@ -41,38 +41,42 @@ import os
 import sys
 from domogik.common.plugin import Plugin
 from domogikmq.pubsub.publisher import MQPub
+from domogikmq.pubsub.subscriber import MQAsyncSub
 import traceback
-import zmq
+#import zmq
 
 # interface vendor id
 INTERFACE_VENDOR_ID = "interface"
 
-class Interface(Plugin):
+class Interface(Plugin, MQAsyncSub):
     '''
     For interfaces clients
     '''
 
 
     def __init__(self, name, stop_cb = None, is_manager = False, parser = None,
-                 daemonize = True, log_prefix = "interface_", test = False, source = None):
+                 daemonize = True, log_prefix = "interface_", log_on_stdout = True, test = False, source = None):
         '''
         Create Interface instance, which defines system handlers
         @param source : overwrite the source value (client-device.instance)
         '''
 
-        Plugin.__init__(self, name, type = "interface", stop_cb = stop_cb, is_manager = is_manager, parser = parser, daemonize = daemonize, log_prefix = log_prefix, test = test)
+        Plugin.__init__(self, name, type = "interface", stop_cb = stop_cb, is_manager = is_manager, parser = parser, daemonize = daemonize, log_prefix = log_prefix, log_on_stdout = log_on_stdout, test = test)
 
         self.log.info(u"Start of the interface init")
         # define the source (in can be used in some plugins)
         if source == None:
             self.source = "{0}-{1}.{2}".format(INTERFACE_VENDOR_ID, self.get_plugin_name(), self.get_sanitized_hostname())
         # in case we overwrite the source : 
+        else:
             self.source = source
 
         ### MQ
         self._mq_name = self.source
-        self.zmq = zmq.Context()
+        #self.zmq = zmq.Context()
         self.mq_pub = MQPub(self.zmq, self._mq_name)
+        # subscribe the MQ for interfaces inputs
+        MQAsyncSub.__init__(self, self.zmq, self._name, ['interface.output'])
 
         ### Context
         # set the context
@@ -83,20 +87,20 @@ class Interface(Plugin):
         # * identity (from face recognition)
         # * mood (from kinect or anything else) 
         # * sex (from voice recognition and post processing on the voice)
-        self.context = {"media" : None,
-                        "location" : None,
-                        "identity" : self.source,
-                        "mood" : None,
-                        "sex" : None
-                       }
+        self.butler_context = {"media" : None,
+                               "location" : None,
+                               "identity" : self.source,
+                               "mood" : None,
+                               "sex" : None
+                              }
         self.log.info(u"End of the interface init")
 
 
     def send_to_butler(self, message):
         """ Send a message over MQ to the butler component
         """
-        self.log.info("Send a message to the butler : {0}".format(message))
-        request = self.context
+        self.log.info(u"Send a message to the butler : {0}".format(message))
+        request = self.butler_context
         request["text"] = message
         self.mq_pub.send_event('interface.input',
                              request)
@@ -106,7 +110,8 @@ class Interface(Plugin):
         """ When a message is received from the MQ (pub/sub)
         """
         if msgid == "interface.output":
-            self.log.debug("interface.output received message : {0}".format(content))
+            self.log.debug(u"interface.output received message : {0}".format(content))
+            self.process_response(content)
             return
 
             ### filter on location, media
@@ -121,4 +126,9 @@ class Interface(Plugin):
             print(u"{0} > {1}".format(BUTLER_CLI_NAME, content['text']))
 
             ### Display the prompt for the next exchange
-            print("{0} > ".format(BUTLER_CLI_USER))
+            print(u"{0} > ".format(BUTLER_CLI_USER))
+
+    def process_response(self, data):
+        """ empty function that should be override to process the butler response
+        """
+        pass
