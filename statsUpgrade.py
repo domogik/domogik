c97fd1b5919c482e4d1fe181713b46604c65c14a
Maikel Punie <maikel.punie@gmail.com>
2015-05-13 16:23:04 +0200

    interface is ready, TODO hook in dbhelper

--- statsUpgrade.py
+++ statsUpgrade.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
 from domogik.common.database import DbHelper
+from domogik import __version__
 
 def corellateOld(devs):
     ret = {}
@@ -17,6 +18,16 @@ def corellateOld(devs):
 
 
 if __name__ == "__main__":
+    print("Welcome to the DOmogik upgrade system")
+    print("This system can upgrade statistics to DOMOGIk 0.4")
+    print("Source: 0.3")
+    print("Destination: 0.4")
+    if __version__ != "0.4.0":
+        print("")
+        print("ERROR: This is only supported in domogik 0.4.0")
+        print("Your domogik version is {0}".format(__version__))
+        print("")
+        exit()
     # 0- connect to the DB
     db = DbHelper()
     db.open_session()
@@ -24,8 +35,12 @@ if __name__ == "__main__":
     # 1- list current devices  (odl ones)
     do = True
     old_devs = corellateOld(db.upgrade_list_old())
+    if len(old_devs) == 0:
+        do = False
+        print("Hooray, all old devices have been uprgaded")
     while do:
-        print old_devs
+        print("Here is a list of all old devices, select the one you want to upgrade")
+        print("")
         # show dev menu
         i = 1
         for dev in old_devs:
@@ -42,9 +57,11 @@ if __name__ == "__main__":
         # we have the device list stats keys
         if sel > 0:
             selDev = old_devs[old_devs.keys()[sel-1]]
-            print("")
             print("Selected Device {0}:".format(selDev['name']))
+            print("")
             j = 1
+            print("Now select the stats key you want to upgrade for this device")
+            print("")
             for k in selDev['keys']:
                 print("{0}. {1}".format(j, k))
                 j = j + 1
@@ -61,20 +78,67 @@ if __name__ == "__main__":
                 print("")
                 print("Select device '{0}' with key '{1}' to upgrade".format(selDev['name'], selKey))
                 print("")
-                print("WARNING:")
-                print("By typing YES below this will start the upgrade process, after the process")
-                print("the upgraded stats keys will be DELETED from the core_device_stats sql tabel.")
-                print("This process can not be reversed, it is advised to make a backup of your")
-                print("datatabse before starting this process")
+
+                print ("Select the sensor to copy the data to")
                 print("")
-                conf = ""
-                while conf not in ["YES I AM SURE", "no"]:
-                    conf = raw_input("Type 'YES I AM SURE' to confirm, 'no' to cancel: ")
-                if conf == "YES I AM SURE":
-                    print("Starting the upgrade")
-                    print ""
-                else:
-                    print("Upgrade CANCELED")
+                newDev = db.list_devices()
+                k = 1
+                for nDev in newDev:
+                    print("{0}. {1}".format(k, nDev["name"]))
+                    k = k +1
+                print("0. Exit")
+                seln = k + 1
+                while seln > k:
+                    try:
+                        seln = int(raw_input('Select Action: '))
+                    except:
+                        print('ERROR: select a number between 0 and {0}. Try again'.format(k))
+                        seln = k + 1
+                if seln > 0:
+                    print("")
+                    print ("Select the sensor to copy the data to")
+                    print("")
+                    l = 1
+                    for nSen in newDev[(seln - 1)]['sensors']:
+                        print("{0}. {1}".format(l, newDev[(seln - 1)]['sensors'][nSen]["name"]))
+                        l = l + 1
+                    print("0. Exit")
+                    sels = l + 1
+                    while sels > l:
+                        try:
+                            sels = int(raw_input('Select Action: '))
+                        except:
+                            print('ERROR: select a number between 0 and {0}. Try again'.format(l))
+                            sels = l + 1
+                    if sels > 0:
+                        l = 1
+                        selS = None
+                        for nSen in newDev[(seln - 1)]['sensors']:
+                            if l == sels:
+                                selS = nSen
+                            l = l + 1
+                        print("")
+                        print("====================================================================================")
+                        print("Upgrade with the folowing info")
+                        print("Old device:      {0}".format(selDev['name']))
+                        print("Old stats key:   {0}".format(selKey))
+                        print("New device:      {0}".format(newDev[(seln - 1)]["name"]))
+                        print("New sensor:      {0}".format(newDev[(seln - 1)]["sensors"][nSen]["name"]))
+                        print("")
+                        print("WARNING:")
+                        print("By typing YES below this will start the upgrade process, after the process")
+                        print("the upgraded stats keys will be DELETED from the core_device_stats sql tabel.")
+                        print("This process can not be reversed, it is advised to make a backup of your")
+                        print("datatabse before starting this process")
+                        print("")
+                        conf = ""
+                        while conf not in ["YES I AM SURE", "no"]:
+                            conf = raw_input("Type 'YES I AM SURE' to confirm, 'no' to cancel: ")
+                        if conf == "YES I AM SURE":
+                            print("Starting the upgrade")
+                            print ""
+                        else:
+                            print("Upgrade CANCELED")
 
         else:
             do = False
@@ -83,10 +147,9 @@ if __name__ == "__main__":
         if do:
             db.open_session()
             old_devs = corellateOld(db.upgrade_list_old())
-
-    # - print done
-    print("")
-    print("Hooray, all old devices have been uprgaded")
+            if len(old_devs) == 0:
+                do = False
+                print("Hooray, all old devices have been uprgaded")
 
     # - Disconnect the db
     del(db)
