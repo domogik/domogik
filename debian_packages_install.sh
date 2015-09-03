def85b5ad6df63a3d16ca8917702b2e8197b57b9
Fritz <fritz.smh@gmail.com>
2015-09-03 15:08:45 +0200

    install : add missing stuff and a small fix

--- debian_packages_install.sh
+++ debian_packages_install.sh
@@ -42,11 +42,12 @@ pkg_list="\
          python-zmq \
          python-gluon \
          python-babel \
+         python-sphinx \
          libusb-1.0-0-dev \
          "
 
-sudo apt-get update
-sudo apt-get install $pkg_list
+apt-get update
+apt-get install $pkg_list
 
 pip_list="Flask-Login \
           Flask-Babel \
@@ -56,7 +57,7 @@ pip_list="Flask-Login \
 
 for elt in $pip_list
   do
-    sudo pip install $elt
+    pip install $elt
 done
 
 
