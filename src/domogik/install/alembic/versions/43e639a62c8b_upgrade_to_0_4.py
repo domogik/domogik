dc436e23f3ad624f73f8254bddf923c7838c85e1
Maikel Punie <maikel.punie@gmail.com>
2015-09-23 16:41:48 +0200

    sync upgrade script to current sql schema

--- src/domogik/install/alembic/versions/43e639a62c8b_upgrade_to_0_4.py
+++ src/domogik/install/alembic/versions/43e639a62c8b_upgrade_to_0_4.py
@@ -24,6 +24,7 @@ def upgrade():
     	sa.Column('device_id', sa.Integer(), nullable=False),
     	sa.Column('key', sa.Unicode(length=32), autoincrement=False, nullable=False),
     	sa.Column('value', sa.Unicode(length=255), nullable=True),
+    	sa.Column('type', sa.Unicode(length=32), nullable=True),
         sa.ForeignKeyConstraint(['device_id'], [u'{0}.id'.format(Device.__tablename__)], ondelete='cascade'),
     	sa.PrimaryKeyConstraint('id'),
     	mysql_engine='InnoDB'
@@ -33,14 +34,20 @@ def upgrade():
     	sa.Column('device_id', sa.Integer(), nullable=False),
     	sa.Column('name', sa.Unicode(length=255), nullable=True),
     	sa.Column('reference', sa.Unicode(length=64), nullable=True),
+        sa.Column('incremental', sa.Boolean(), nullable=False),
+        sa.Column('formula', sa.UnicodeText()),
     	sa.Column('data_type', sa.Unicode(length=32), nullable=False),
     	sa.Column('conversion', sa.Unicode(length=255), nullable=True),
     	sa.Column('last_value', sa.Unicode(length=32), nullable=True),
     	sa.Column('last_received', sa.Integer(), nullable=True),
+        sa.Column('value_min', sa.Float(), nullable=True),
+        sa.Column('value_max', sa.Float(), nullable=True),
         sa.Column('history_store', sa.Boolean(), nullable=False),
         sa.Column('history_max', sa.Integer(), nullable=True),
         sa.Column('history_expire', sa.Integer(), nullable=True),
         sa.Column('history_round', sa.Float(), nullable=True),
+        sa.Column('history_duplicate', sa.Boolean(), nullable=False),
+        sa.Column('timeout', sa.Integer(), nullable=True),
         sa.ForeignKeyConstraint(['device_id'], [u'{0}.id'.format(Device.__tablename__)], ondelete='cascade'),
     	sa.PrimaryKeyConstraint('id'),
     	mysql_engine='InnoDB'
@@ -71,6 +78,7 @@ def upgrade():
     	sa.Column('date', sa.DateTime(), nullable=False, index=True),
     	sa.Column('value_num', sa.Float(), nullable=True),
     	sa.Column('value_str', sa.Unicode(length=32), nullable=False),
+    	sa.Column('original_value_num', sa.Float(), nullable=True),
     	sa.ForeignKeyConstraint(['sensor_id'], [u'{0}.id'.format(Sensor.__tablename__)], ondelete='cascade'),
     	sa.PrimaryKeyConstraint('id'),
     	mysql_engine='InnoDB'
@@ -110,7 +118,7 @@ def upgrade():
     	sa.ForeignKeyConstraint(['cmd_id'], [u'{0}.id'.format(Command.__tablename__)], ondelete='cascade'),
     	sa.PrimaryKeyConstraint('cmd_id', 'key'),
     	mysql_engine='InnoDB'
-    )
+        )
     op.create_table(XplCommandParam.__tablename__,
 	sa.Column('xplcmd_id', sa.Integer(), nullable=False),
     	sa.Column('key', sa.Unicode(length=32), autoincrement=False, nullable=False),
@@ -123,6 +131,7 @@ def upgrade():
     #op.drop_constraint('core_device_ibfk_2', Device.__tablename__, type_='foreignkey')
     #op.drop_constraint('core_device_type_ibfk_1', 'core_device_type', type_='foreignkey')
     op.add_column(Device.__tablename__, sa.Column('client_id', sa.Unicode(length=80), nullable=False))
+    op.add_column(Device.__tablename__, sa.Column('client_version', sa.Unicode(length=32), nullable=False))
     op.drop_column(Device.__tablename__, u'device_usage_id')
     op.alter_column(Device.__tablename__, u'address',
                existing_type=mysql.VARCHAR(length=255),
