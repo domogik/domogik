18020ca66aedf0a6c8d4201bb735d1efbc68b70a
Fritz <fritz.smh@gmail.com>
2016-02-11 23:18:47 +0100

    scenario : remove useless print

--- src/domogik/scenario/actions/abstract.py
+++ src/domogik/scenario/actions/abstract.py
@@ -138,9 +138,7 @@ class AbstractAction:
         # local variables
         try:
             for a_var in local_vars:
-                print(u"=> {0}".format(a_var))
                 input = input.replace(u"{{{{{0}}}}}".format(a_var), local_vars[a_var])
-                print(input)
         except:
             self._log.error(u"Error while replacing local variables '{0}' in '{1}'. Error is : {2}".format(local_vars, input, traceback.format_exc()))
         return input
