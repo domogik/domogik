2bdc67d66e70ffd51000029c32673cdeedc1aadf
Fritz <fritz.smh@gmail.com>
2016-04-28 22:07:51 +0200

    Fix dmg_version

--- src/domogik/bin/version.py
+++ src/domogik/bin/version.py
@@ -35,25 +35,25 @@ Implements
 @license: GPL(v3)
 @organization: Domogik
 """
-from domogik.bin.rest import REST_API_VERSION
-import domogik.rest
+#from domogik.bin.admin import REST_API_VERSION
+#import domogik.admin.rest
 import sys
 import os
 from subprocess import Popen, PIPE
 
 
-def rest_status_src_release():
-    """ Return sources release
-    """
-    domogik_path = os.path.dirname(domogik.rest.__file__)
-    subp = Popen("cd {0} ; hg branch | xargs hg log -l1 --template '{branch}.{rev} ({latesttag}) - {date|isodate}' -b".format(domogik_path), shell=True, stdout=PIPE, stderr=PIPE)
-    (stdout, stderr) = subp.communicate()
-    # if hg id has no error, we are using source  repository
-    if subp.returncode == 0:
-        return "{0}".format(stdout)
-    # else, we send dmg release
-    else:
-        return rest_status_dmg_release()
+#def rest_status_src_release():
+#    """ Return sources release
+#    """
+#    domogik_path = os.path.dirname(domogik.admin.rest.__file__)
+#    subp = Popen("cd {0} ; hg branch | xargs hg log -l1 --template '{branch}.{rev} ({latesttag}) - {date|isodate}' -b".format(domogik_path), shell=True, stdout=PIPE, stderr=PIPE)
+#    (stdout, stderr) = subp.communicate()
+#    # if hg id has no error, we are using source  repository
+#    if subp.returncode == 0:
+#        return "{0}".format(stdout)
+#    # else, we send dmg release
+#    else:
+#        return rest_status_dmg_release()
 
 def rest_status_dmg_release():
     """ Return Domogik release
@@ -64,9 +64,9 @@ def rest_status_dmg_release():
 
 
 def main():
-    print(u"REST_API_release : {0}".format(REST_API_VERSION))
+#    print(u"REST_API_release : {0}".format(REST_API_VERSION))
     print(u"Domogik_release : {0}".format(rest_status_dmg_release()))
-    print(u"Sources_release : {0}".foramt(rest_status_src_release()))
+#    print(u"Sources_release : {0}".format(rest_status_src_release()))
 
 
 if __name__ == "__main__":
