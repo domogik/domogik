ed73089298a05e4f982cef6a7dfd61e707da4fae
Fritz <fritz.smh@gmail.com>
2016-11-22 22:17:47 +0100

    newdoc : add missing files for travis/RTD

---
+++ docs/conf.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+#
+
+import sys, os
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinxcontrib.blockdiag', 'sphinxcontrib.nwdiag', 'sphinxcontrib.seqdiag', 'sphinxcontrib.actdiag']
+#templates_path = ['_templates']
+source_suffix = '.txt'
+master_doc = 'contents'
+
+# General information about the project.
+project = u'Domogik'
+copyright = u'2016, Domogik'
+
+version = '0.5'
+release = '0.5'
+
+exclude_patterns = ['_build']
+pygments_style = 'sphinx'
+todo_include_todos=True
+
+html_theme = 'domogikdoc'
+#html_theme_path = ["_theme", "../../../sphinx/_theme"]
+html_static_path = []
+
+
+latex_elements = {
+}
+
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title, author, documentclass [howto/manual]).
+latex_documents = [
+  ('index', 'Domogik.tex', u'Domogik Documentation',
+   u'Domogik team', 'manual'),
+]
+
+man_pages = [
+    ('index', 'domogik', u'Domogik Documentation',
+     [u'Domogik team'], 1)
+]
+
+texinfo_documents = [
+  ('index', 'Domogik', u'Domogik Documentation',
+   u'Domogik team', 'Domogik', 'One line description of project.',
+   'Miscellaneous'),
+]
+
