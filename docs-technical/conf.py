a41f095f761967659e78569d032bb0cba64f20ce
Fritz <fritz.smh@gmail.com>
2016-12-01 22:37:11 +0100

    some doc tests

--- docs-technical/conf.py
+++ docs-technical/conf.py
@@ -17,7 +17,8 @@ import sys, os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
-sys.path.append(os.path.abspath('../docs-technical'))
+#sys.path.append(os.path.abspath('../docs-technical'))
+sys.path.insert(0, os.path.abspath('../docs-technical'))
 
 # -- General configuration -----------------------------------------------------
 
