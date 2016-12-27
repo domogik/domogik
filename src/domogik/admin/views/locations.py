072342ddcd1da30f8e88cab92dbd41f5db4db493
Maikel Punie <maikel.punie@gmail.com>
2016-12-27 20:28:20 +0100

    Locations overview page is finished

--- src/domogik/admin/views/locations.py
+++ src/domogik/admin/views/locations.py
@@ -4,9 +4,10 @@ from domogik.admin.application import app, render_template
 @app.route('/locations')
 @login_required
 def locations():
-    return render_template('locations.html',
-        locations = {},
-        mactive = "locations")
+    with app.db.session_scope():
+        return render_template('locations.html',
+            locations = app.db.get_all_location(),
+            mactive = "locations")
 
 @app.route('/locations/edit/<id>', methods=['GET', 'POST'])
 @login_required
