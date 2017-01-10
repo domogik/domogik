5d0905b6d52c2a7b31b3d73561db52cd547794c8
Maikel Punie <maikel.punie@gmail.com>
2017-01-10 05:37:34 +0100

    Orphans should not show the internal (client_id=core) devices

--- src/domogik/admin/views/orphans.py
+++ src/domogik/admin/views/orphans.py
@@ -23,7 +23,7 @@ def orphans():
     # loop over the devices
     orphan_devs = []
     for dev in devs:
-        if dev["client_id"] not in list(client_list.keys()):
+        if dev["client_id"] not in list(client_list.keys()) and dev['client_id'] != 'core':
             orphan_devs.append(dev)
 
     return render_template('orphans.html',
