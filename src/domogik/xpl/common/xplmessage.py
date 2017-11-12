b5740835ae8d0c0b7fdfc8c54da09e5b06971b49
Maikel Punie <maikel.punie@gmail.com>
2017-11-12 11:52:09 +0100

    Try to fix py2 in this branch

--- src/domogik/xpl/common/xplmessage.py
+++ src/domogik/xpl/common/xplmessage.py
@@ -73,7 +73,7 @@ Implements
 @license: GPL(v3)
 @organization: Domogik
 """
-from __future__ import absolute_import, division, print_function, unicode_literals
+from __future__ import absolute_import, division, print_function
 import re
 import copy
 
