13cdc4685b7fc89ce14497360e2093634dddf6ab
Maikel Punie <maikel.punie@gmail.com>
2017-11-12 11:56:47 +0100

    Try to fix py2 in this branch

--- src/domogik/tests/bin/testrunner.py
+++ src/domogik/tests/bin/testrunner.py
@@ -215,7 +215,7 @@ def main():
         testr.run_testcases()
         cr = testr.get_result()
     except Exception as exp:
-        print exp
+        print(exp)
         cr = 1
     sys.exit(cr)
 
