b4f5321fe2879491fb339711ca14f1f9c3a488c7
Maikel Punie <maikel.punie@gmail.com>
2017-11-14 16:09:09 +0100

    another try

--- src/domogik/tests/travis/travis-install.sh
+++ src/domogik/tests/travis/travis-install.sh
@@ -2,19 +2,19 @@
 # The -e option will make the bash stop if any command raise an error ($? != 0)
 
 echo "==== SOME SETUP stuff"
-sudo mkdir -p /var/lock/domogik
-sudo chown $LOGNAME:root /var/lock/domogik
-sudo touch /var/lock/domogik/config.lock
-sudo chown $LOGNAME:root /var/lock/domogik/config.lock
+mkdir -p /var/lock/domogik
+chown $LOGNAME:root /var/lock/domogik
+touch /var/lock/domogik/config.lock
+chown $LOGNAME:root /var/lock/domogik/config.lock
 
 echo "==== RUNNING installer"
 wget https://raw.githubusercontent.com/domogik/domogik-installation/master/install-develop.sh -O /tmp/install.sh
 chmod +x /tmp/install.sh
-sudo /tmp/install.sh
+/tmp/install.sh
 
 echo "==== SOME CLEANUP stuff"
-sudo chown $LOGNAME:root /var/lock/domogik
-sudo chown $LOGNAME:root /var/lock/domogik/config.lock
+chown $LOGNAME:root /var/lock/domogik
+chown $LOGNAME:root /var/lock/domogik/config.lock
 #sudo chown $LOGNAME:root /var/log/domogik/*
 ls -l /var/lock/domogik/
 
