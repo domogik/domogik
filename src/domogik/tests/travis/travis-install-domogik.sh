675af8c8cd50d4b575481f254fdcb2ce45485275
Maikel Punie <maikel.punie@gmail.com>
2017-12-04 06:30:17 +0100

    try to copyover the virtual env step2

--- src/domogik/tests/travis/travis-install-domogik.sh
+++ src/domogik/tests/travis/travis-install-domogik.sh
@@ -1,13 +1,17 @@
 #!/bin/bash -e 
 # The -e option will make the bash stop if any command raise an error ($? != 0)
 
+echo "==== Downloading and preparing the install script"
 wget https://raw.githubusercontent.com/domogik/domogik-installation/master/install-develop.sh -O /tmp/install.sh
 chmod +x /tmp/install.sh
+
+echo "==== Copy over the virtual env to root"
 export
 VENV=$VIRTUAL_ENV
-sudo su
-source VENV/bin/activate
-/tmp/install.sh
+echo $VENV
+
+echo "==== Launch the installer"
+sudo "source VENV/bin/activate; /tmp/install.sh"
 
 echo "==== SOME CLEANUP stuff"
 sudo chown $LOGNAME:root /var/lock/domogik
