651d5b45e4e4bf42a58e6435ab838d94e2e16505
Nico0084 <nico84dev@gmail.com>
2019-04-29 16:31:52 +0200

    Travis try setup database

--- src/domogik/tests/travis/travis-setup-database.sh
+++ src/domogik/tests/travis/travis-setup-database.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e 
 # The -e option will make the bash stop if any command raise an error ($? != 0)
 
-mysql -e 'create database domogik;'
-echo "USE mysql;\nUPDATE user SET password=PASSWORD('domopass') WHERE user='travis';\nFLUSH PRIVILEGES;\n" | mysql -u root
+mysql -e 'CREATE DATABASE domogik;'
+sudo mysql -e "use mysql; update user set authentication_string=PASSWORD('domopass') where User='travis'; update user set plugin='mysql_native_password';FLUSH PRIVILEGES;"
+sudo mysql_upgrade -u travis -pdomopass
 echo "GRANT ALL PRIVILEGES ON domogik.* TO 'travis'@'%' WITH GRANT OPTION;\n" | mysql -u root
-
+sudo service mysql restart
