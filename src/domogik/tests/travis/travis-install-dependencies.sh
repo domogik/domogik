93c1bcbe8e10eee06b5bf409913616e7378ca73b
Nico0084 <nico84dev@gmail.com>
2019-06-12 15:51:33 +0000

    travis : use python3.6, zmq4

--- src/domogik/tests/travis/travis-install-dependencies.sh
+++ src/domogik/tests/travis/travis-install-dependencies.sh
@@ -2,10 +2,10 @@
 # The -e option will make the bash stop if any command raise an error ($? != 0)
 
 sudo apt-get update
-sudo apt-get install libzmq3-dev
+sudo apt-get install libzmq4-dev
 pip install pyzmq
 pip install argparse
-sudo apt-get install python2.7-dev gcc
+sudo apt-get install python3.6-dev gcc
 sudo apt-get install libssl-dev
 sudo apt-get install libmysqlclient-dev #mysql-client
 pip install psycopg2
