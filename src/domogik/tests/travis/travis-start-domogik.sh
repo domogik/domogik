3c348da6caf8b5a7809b65ea3b5f25c11dbeae25
Nico0084 <nico84dev@gmail.com>
2019-10-18 16:16:17 +0000

    Travis: sudo start domogik

--- src/domogik/tests/travis/travis-start-domogik.sh
+++ src/domogik/tests/travis/travis-start-domogik.sh
@@ -1,13 +1,9 @@
 #!/bin/bash -e
 # The -e option will make the bash stop if any command raise an error ($? != 0)
 
-# No needed, allready activate by travis
-#echo "=== Start virtualenv dmg_==="
-#source /home/travis/virtualenv/python$PYTHON_VER/bin/activate
-
 #sleep 10
 echo "==== START DOMOGIK"
-/etc/init.d/domogik start
+sudo /etc/init.d/domogik start
 sleep 10
 cat /var/log/domogik/core_manager.log
 
