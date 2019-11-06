5f81134d7e67fe3414c441f6e9c21979768f6327
Nico0084 <nico84dev@gmail.com>
2019-11-06 18:34:32 +0000

    Travis: try understang package issue

--- install.py
+++ install.py
@@ -437,6 +437,8 @@ def find_interface():
     try:
         import traceback
 
+        for d in pkg_resources.working_set :
+            print(d)
         pkg_resources.get_distribution("Domogik").activate()
         from domogik.common.utils import get_interfaces, interface_has_ip
         for intf in get_interfaces():
