3ba6ff231b15fc5658c2d699838fc262d7223524
Nico0084 <nico84dev@gmail.com>
2019-11-07 18:57:02 +0000

    Travis: try acttivate sudo

--- src/domogik/tests/travis/travis-install-domogik.sh
+++ src/domogik/tests/travis/travis-install-domogik.sh
@@ -18,7 +18,7 @@ echo "==== RUNNING INSTALL.py"
 # notice : we use --no-setup and --no-db-upgrade because a virtualenv is used in Travis.
 # If we don't set these flags, these actions will be done as root user and so,
 # the python packages (domogik) in virtualenv won't be find!
-sudo -E su $USER -c python3 install.py --no-setup --no-test --no-db-upgrade --user $LOGNAME --command-line --domogik_log_level debug --domogik_bind_interface lo --database_name domogik --database_user travis --admin_interfaces lo --admin_clean_json True --hub_interfaces lo --hub_log_level info --hub_log_bandwidth True --hub_log_invalid_data True --no-mq-check --metrics_id travis_build
+sudo -E python3 install.py --no-setup --no-test --no-db-upgrade --user $LOGNAME --command-line --domogik_log_level debug --domogik_bind_interface lo --database_name domogik --database_user travis --admin_interfaces lo --admin_clean_json True --hub_interfaces lo --hub_log_level info --hub_log_bandwidth True --hub_log_invalid_data True --no-mq-check --metrics_id travis_build
 
 echo "==== RUNNING DB_INSTALL.py"
 python3 src/domogik/install/db_install.py
@@ -28,5 +28,3 @@ sudo chown $LOGNAME:root /var/lock/domogik
 sudo chown $LOGNAME:root /var/lock/domogik/config.lock
 #sudo chown $LOGNAME:root /var/log/domogik/*
 ls -l /var/lock/domogik/
-
-
