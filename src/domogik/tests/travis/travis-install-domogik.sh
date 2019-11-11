b20c91967e5fa28747139e6f4d6f72e9210d402e
Nico0084 <nico84dev@gmail.com>
2019-11-11 18:43:21 +0000

    Travis: Create dir before install

--- src/domogik/tests/travis/travis-install-domogik.sh
+++ src/domogik/tests/travis/travis-install-domogik.sh
@@ -2,10 +2,17 @@
 # The -e option will make the bash stop if any command raise an error ($? != 0)
 
 echo "==== SOME SETUP stuff"
+PIDDIR="/var/run/domogik"
+LOCKDIR="/var/lock/domogik"
+LOGDIR="/var/log/domogik"
 sudo mkdir -p /var/lock/domogik
-sudo chown $LOGNAME:root /var/lock/domogik
-sudo touch /var/lock/domogik/config.lock
-sudo chown $LOGNAME:root /var/lock/domogik/config.lock
+sudo chown $LOGNAME:root $LOCKDIR
+sudo touch $LOCKDIR/config.lock
+sudo chown $LOGNAME:root $LOCKDIR/config.lock
+sudo mkdir $PIDDIR
+sudo chown $LOGNAME: $PIDDIR
+sudo mkdir $LOGDIR
+sudo chown $LOGNAME: $LOGDIR
 
 echo "==== RUNNING SETUP.py"
 pip3 install .
@@ -18,6 +25,7 @@ echo "==== RUNNING INSTALL.py"
 # notice : we use --no-setup and --no-db-upgrade because a virtualenv is used in Travis.
 # If we don't set these flags, these actions will be done as root user and so,
 # the python packages (domogik) in virtualenv won't be find!
+
 python3 install.py --no-setup --no-test --no-db-upgrade --user $LOGNAME --command-line --domogik_log_level debug --domogik_bind_interface lo --database_name domogik --database_user travis --admin_interfaces lo --admin_clean_json True --hub_interfaces lo --hub_log_level info --hub_log_bandwidth True --hub_log_invalid_data True --no-mq-check --metrics_id travis_build
 
 echo "==== RUNNING DB_INSTALL.py"
