bf313e0a4fba7d66e5be33705babfaa7ba1fc8ef
Nico0084 <nico84dev@gmail.com>
2019-11-12 23:14:37 +0000

    Travis: fix dmg_hub, display which

--- src/domogik/tests/travis/travis-install-domogik.sh
+++ src/domogik/tests/travis/travis-install-domogik.sh
@@ -28,7 +28,7 @@ sudo chown $LOGNAME:root /var/lock/domogik
 sudo chown $LOGNAME:root /var/lock/domogik/config.lock
 #sudo chown $LOGNAME:root /var/log/domogik/*
 ls -l /var/lock/domogik/
-echo "==== PATH"
+echo "==== $VIRTUAL_ENV/bin/"
 echo $PATH
 dmg_version
 ls -l $VIRTUAL_ENV/bin/
